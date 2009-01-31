597ab54d5ca73a8f7553c9e5050f59ffb7be1b3f
ljosa <ljosa@1fc53939-2000-0410-845c-e8453a809027>
2009-01-31 15:44:57 +0000

    Added comment.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CPAnalyst@6819 1fc53939-2000-0410-845c-e8453a809027

--- src/FastGentleBoostingMulticlass.py
+++ src/FastGentleBoostingMulticlass.py
@@ -7,6 +7,7 @@ def train(colnames, num_learners, label_matrix, values, fout=None, do_prof=False
     label_matrix is an n by k numpy array containing values of either +1 or -1
     values is the n by j numpy array of cell measurements
     n = #example cells, k = #classes, j = #measurements
+    Return a list of learners.  Each learner is a tuple (column, thresh, a, b).
     '''
     computed_labels = zeros(label_matrix.shape, float32)
     num_examples, num_classes = label_matrix.shape
