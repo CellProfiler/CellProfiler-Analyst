5a1795a66a9e7bf3614a01fe993b05812affa3b5
afraser <afraser@1fc53939-2000-0410-845c-e8453a809027>
2009-03-17 18:39:57 +0000

    PlateMapPanel shape is now (rows, cols) inferred from numpy array or set manually.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CPAnalyst@7096 1fc53939-2000-0410-845c-e8453a809027

--- src/PlateMapPanel.py
+++ src/PlateMapPanel.py
@@ -17,8 +17,8 @@ class PlateMapPanel(wx.Panel):
         self.colormap = colormap
         self.SetData(data, shape)
         self.selection = set([])
-        self.x_labels = ['%02d'%i for i in range(1,self.data.shape[0]+1)]
-        self.y_labels = ['%2s'%c for c in alphabet[:self.data.shape[1]]]
+        self.row_labels = ['%2s'%c for c in alphabet[:self.data.shape[0]]]
+        self.col_labels = ['%02d'%i for i in range(1,self.data.shape[1]+1)]
         self.Bind(wx.EVT_PAINT, self.OnPaint)
         self.Bind(wx.EVT_LEFT_DOWN, self.OnClick)
        
@@ -27,7 +27,7 @@ class PlateMapPanel(wx.Panel):
         '''
         data: An iterable with values between 0 and 1. It's shape will be used
               to layout the plate unless overridden by the shape parameter
-        shape: If passed, this will be used to reshape the data.
+        shape: If passed, this will be used to reshape the data. (rows,cols)
         '''
         self.data = np.array(data).astype('float')
         if shape != None:
@@ -36,15 +36,15 @@ class PlateMapPanel(wx.Panel):
         self.Refresh()
         
     
-    def SetXLabels(self, labels):
-        assert len(labels) >= self.data.shape[0]
-        self.x_labels = ['%2s'%c for c in labels]
+    def SetColLabels(self, labels):
+        assert len(labels) >= self.data.shape[1]
+        self.col_labels = ['%2s'%c for c in labels]
         self.Refresh()
         
     
-    def SetYLabels(self, labels):
-        assert len(labels) >= self.data.shape[1]
-        self.y_labels = ['%2s'%c for c in labels]
+    def SetRowLabels(self, labels):
+        assert len(labels) >= self.data.shape[0]
+        self.row_labels = ['%2s'%c for c in labels]
         self.Refresh()
         
         
@@ -54,13 +54,13 @@ class PlateMapPanel(wx.Panel):
         
     
     def SelectWell(self, well):
-        ''' well: 2-tuple of integers indexing a well position '''
+        ''' well: 2-tuple of integers indexing a well position (row,col)'''
         self.selection = set([well])
         self.Refresh()
         
         
     def ToggleSelected(self, well):
-        ''' well: 2-tuple of integers indexing a well position '''
+        ''' well: 2-tuple of integers indexing a well position (row,col)'''
         if well in self.selection:
             self.selection.remove(well)
         else:
@@ -71,11 +71,11 @@ class PlateMapPanel(wx.Panel):
     def GetWellAtCoord(self, x, y):
         ''' returns a 2 tuple of integers indexing a well position 
                     or -1 if there is no well at the given position. '''
-        r = min(self.Size[0]/(self.data.shape[0]+1.)/2.,
-                self.Size[1]/(self.data.shape[1]+1.)/2.) - 1.
+        r = min(self.Size[0]/(self.data.shape[1]+1.)/2.,
+                self.Size[1]/(self.data.shape[0]+1.)/2.) - 1.
         i = int((x-2)/(r*2+2))
         j = int((y-2)/(r*2+2))
-        if 0<i<=self.data.shape[0] and 0<j<=self.data.shape[1]:
+        if 0<i<=self.data.shape[1] and 0<j<=self.data.shape[0]:
             return (i-1,j-1)
         else:
             return -1
@@ -85,27 +85,27 @@ class PlateMapPanel(wx.Panel):
         dc = wx.PaintDC(self)
         dc.Clear()
         dc.BeginDrawing()
-        r = min(self.Size[0]/(self.data.shape[0]+1.)/2.,
-                self.Size[1]/(self.data.shape[1]+1.)/2.) - 1.
+        r = min(self.Size[0]/(self.data.shape[1]+1.)/2.,
+                self.Size[1]/(self.data.shape[0]+1.)/2.) - 1.
         dc.SetPen(wx.Pen("BLACK",min(r/10., 1.)))
         py = 0
         i=0
-        for y in range(self.data.shape[1]+1):
+        for y in range(self.data.shape[0]+1):
             px = 0
-            for x in range(self.data.shape[0]+1):
+            for x in range(self.data.shape[1]+1):
                 # Draw column headers
                 if y==0 and x!=0:
-                    dc.DrawText(self.x_labels[x-1], px+r/2, py+r/2)
+                    dc.DrawText(self.col_labels[x-1], px+r/2, py+r/2)
                 # Draw row headers
                 elif y!=0 and x==0:
-                    dc.DrawText(self.y_labels[y-1], px+r/2, py+r/2)
+                    dc.DrawText(self.row_labels[y-1], px+r/2, py+r/2)
                 # Draw wells
                 elif y>0 and x>0:
                     if (x-1,y-1) in self.selection:
                         dc.SetPen(wx.Pen("BLACK",5))
                     else:
                         dc.SetPen(wx.Pen("BLACK",min(r/10., 1.)))
-                    color = np.array(self.colormap(self.data[x-1][y-1])[:3]) * 255
+                    color = np.array(self.colormap(self.data[y-1][x-1])[:3]) * 255
                     dc.SetBrush(wx.Brush(color))
                     dc.DrawRoundedRectangle(px+1, py+1, r*2, r*2, r*0.75)
 #                    dc.DrawCircle(px+r+1, py+r+1, r)
@@ -122,9 +122,10 @@ class PlateMapPanel(wx.Panel):
         if evt.ShiftDown():
             self.ToggleSelected(self.GetWellAtCoord(evt.X, evt.Y))
         else:
-            self.SelectWell(self.GetWellAtCoord(evt.X, evt.Y))        
+            self.SelectWell(self.GetWellAtCoord(evt.X, evt.Y))
    
-   
+
+
    
 
 if __name__ == "__main__":
@@ -132,12 +133,12 @@ if __name__ == "__main__":
     
     data = np.arange(384)/384.
     frame = wx.Frame(None, size=(600.,430.))
-    p = PlateMapPanel(frame, data, shape=(24,16))
+    p = PlateMapPanel(frame, data, shape=(16,24))
     frame.Show()
     
 #    f = wx.Frame(None)
 #    data = np.arange(96)/96.
-#    p2 = PlateMapPanel(f, data, (12,8)) #96 wells
+#    p2 = PlateMapPanel(f, data, (8,12)) #96 wells
 #    f.Show()
     
     app.MainLoop()
