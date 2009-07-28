ff96d2ebb2fb65fe61e2dd2bdcc6e13d6a8d27dd
thouis <thouis@1fc53939-2000-0410-845c-e8453a809027>
2009-07-28 22:01:10 +0000

    more matplotlib py2exe changes
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CPAnalyst@7813 1fc53939-2000-0410-845c-e8453a809027

--- src/setup_py2exe.py
+++ src/setup_py2exe.py
@@ -5,7 +5,7 @@ import matplotlib
 setup(console=['ClassifierGUI.py'],
       options={
         'py2exe': {
-            'packages' : ['matplotlib', 'pytz'],
+            'packages' : ['matplotlib', 'pytz', 'MySQLdb'],
             "excludes": ['_gtkagg', '_tkagg'],
             "dll_excludes": ['libgdk-win32-2.0-0.dll',
                              'libgobject-2.0-0.dll', 
