69512cb0cd657edb8b7e42a7e43801b56a55398e
thouis <thouis@1fc53939-2000-0410-845c-e8453a809027>
2009-07-29 14:59:42 +0000

    experimenting with smaller package
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CPAnalyst@7814 1fc53939-2000-0410-845c-e8453a809027

--- src/setup_py2exe.py
+++ src/setup_py2exe.py
@@ -2,14 +2,15 @@ from distutils.core import setup
 import py2exe
 import matplotlib
 
-setup(console=['ClassifierGUI.py'],
+setup(console=['ClassifierGUI.pyw'],
       options={
         'py2exe': {
-            'packages' : ['matplotlib', 'pytz', 'MySQLdb'],
-            "excludes": ['_gtkagg', '_tkagg'],
+            'packages' : ['matplotlib', 'pytz', 'MySQLdb', 'sqlite3'],
+            "excludes": ['_gtkagg', '_tkagg', 
+                         "Tkconstants","Tkinter","tcl"],
             "dll_excludes": ['libgdk-win32-2.0-0.dll',
                              'libgobject-2.0-0.dll', 
-                             'libgdk_pixbuf-2.0-0.dll',]
+                             'libgdk_pixbuf-2.0-0.dll']
             }
         },
       data_files=matplotlib.get_py2exe_datafiles(),
