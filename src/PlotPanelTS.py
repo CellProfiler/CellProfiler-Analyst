f49417259c6d4e00058266f6947c2b72a4999e84
tshah <tshah@1fc53939-2000-0410-845c-e8453a809027>
2009-08-11 15:30:55 +0000

    Added PlotPanelTS, a panel that holds matplotlib figures, and CellDistributionViewer, a class to view 2D frequency plots of cell properties
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CPAnalyst@7861 1fc53939-2000-0410-845c-e8453a809027

---
+++ src/PlotPanelTS.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+
+import matplotlib
+matplotlib.interactive( True )
+matplotlib.use( 'WXAgg' )
+
+import numpy as num
+import wx
+
+class PlotPanel (wx.Panel):
+    """The PlotPanel has a Figure and a Canvas. OnSize events simply set a 
+flag, and the actual resizing of the figure is triggered by an Idle event."""
+    def __init__( self, parent, color=None, dpi=None, **kwargs ):
+        from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
+        from matplotlib.figure import Figure
+
+        # initialize Panel
+        if 'id' not in kwargs.keys():
+            kwargs['id'] = wx.ID_ANY
+        if 'style' not in kwargs.keys():
+            kwargs['style'] = wx.NO_FULL_REPAINT_ON_RESIZE
+        wx.Panel.__init__( self, parent, **kwargs )
+
+        # initialize matplotlib stuff
+        self.figure = Figure( None, dpi )
+        self.canvas = FigureCanvasWxAgg( self, -1, self.figure )
+        self.SetColor( color )
+
+        self._SetSize()
+        self.draw()
+
+        self._resizeflag = False
+
+        self.Bind(wx.EVT_IDLE, self._onIdle)
+        self.Bind(wx.EVT_SIZE, self._onSize)
+
+    def SetColor( self, rgbtuple=None ):
+        """Set figure and canvas colours to be the same."""
+        if rgbtuple is None:
+            rgbtuple = wx.SystemSettings.GetColour( wx.SYS_COLOUR_BTNFACE ).Get()
+        clr = [c/255. for c in rgbtuple]
+        self.figure.set_facecolor( clr )
+        self.figure.set_edgecolor( clr )
+        self.canvas.SetBackgroundColour( wx.Colour( *rgbtuple ) )
+
+    def _onSize( self, event ):
+        self._resizeflag = True
+
+    def _onIdle( self, evt ):
+        if self._resizeflag:
+            self._resizeflag = False
+            self._SetSize()
+
+    def _SetSize( self ):
+        pixels = tuple( self.parent.GetClientSize() )       
+        self.SetSize( pixels )
+        self.canvas.SetSize( pixels )
+        self.figure.set_size_inches( float( pixels[0] )/self.figure.get_dpi(),
+                                     float( pixels[1] )/self.figure.get_dpi() )
+
+    def draw(self): pass # abstract, to be overridden by child classes
+
+
+class FigurePanel(PlotPanel):
+    def __init__( self, parent, point_lists, clr_list, **kwargs ):
+        self.parent = parent
+        self.point_lists = point_lists
+        self.clr_list = clr_list
+
+        # initiate plotter
+        PlotPanel.__init__( self, parent, **kwargs )
+        self.SetColor( (255,255,255) )
+    
+    
+    def setpointslists(self,points):
+        self.point_lists = points
+    
+    def getpointslists(self):
+        return self.point_lists
+    
+    
+    def draw( self ):
+        #Draw data.
+        if not hasattr( self, 'subplot' ):
+            self.subplot = self.figure.add_subplot( 111 )
+
+        self.subplot.clear()
+
+        for i, pt_list in enumerate( self.point_lists ):
+            plot_pts = num.array( pt_list )
+            clr = [float( c )/255. for c in self.clr_list[i]]
+            #self.subplot.plot( plot_pts[:,0], plot_pts[:,1], color=clr )
+            self.subplot.scatter( plot_pts[:,0], plot_pts[:,1], color=clr )
+
+
+
+"""
+if __name__ == '__main__':
+    class DemoPlotPanel (PlotPanel):
+        #Plots several lines in distinct colors.
+        def __init__( self, parent, point_lists, clr_list, **kwargs ):
+            self.parent = parent
+            self.point_lists = point_lists
+            self.clr_list = clr_list
+
+            # initiate plotter
+            PlotPanel.__init__( self, parent, **kwargs )
+            self.SetColor( (255,255,255) )
+
+        def draw( self ):
+            #Draw data.
+            if not hasattr( self, 'subplot' ):
+                self.subplot = self.figure.add_subplot( 111 )
+
+            for i, pt_list in enumerate( self.point_lists ):
+                plot_pts = num.array( pt_list )
+                clr = [float( c )/255. for c in self.clr_list[i]]
+                self.subplot.plot( plot_pts[:,0], plot_pts[:,1], color=clr )
+
+
+    class GUIPanel (wx.Panel):
+        def __init__( self, parent, point_lists, clr_list, **kwargs ):
+            self.parent = parent
+            self.point_lists = point_lists
+            self.clr_list = clr_list
+            
+            self.control = wx.TextCtrl(parent, 1, style=wx.TE_MULTILINE)
+            self.sizer=wx.BoxSizer(wx.VERTICAL)
+            self.sizer.Add(self.control,1,wx.EXPAND)
+            self.plotpanel = DemoPlotPanel(parent, point_lists, clr_list )
+            self.sizer.Add(parent,0,wx.EXPAND)
+            self.parent.SetSizer(self.sizer)
+            self.parent.SetAutoLayout(1)
+            self.sizer.Fit(self.parent)
+
+
+
+    theta = num.arange( 0, 45*2*num.pi, 0.02 )
+
+    rad0 = (0.8*theta/(2*num.pi) + 1)
+    r0 = rad0*(8 + num.sin( theta*7 + rad0/1.8 ))
+    x0 = r0*num.cos( theta )
+    y0 = r0*num.sin( theta )
+
+    rad1 = (0.8*theta/(2*num.pi) + 1)
+    r1 = rad1*(6 + num.sin( theta*7 + rad1/1.9 ))
+    x1 = r1*num.cos( theta )
+    y1 = r1*num.sin( theta )
+
+    points = [[(xi,yi) for xi,yi in zip( x0, y0 )],
+              [(xi,yi) for xi,yi in zip( x1, y1 )]]
+    clrs = [[225,200,160], [219,112,147]]
+
+    app = wx.PySimpleApp( 0 )
+    frame = wx.Frame( None, wx.ID_ANY, 'WxPython and Matplotlib', size=(300,300) )
+    frame.CreateStatusBar()
+    
+    panel = GUIPanel( frame, points, clrs )
+    frame.Show()
+    app.MainLoop()
+"""
