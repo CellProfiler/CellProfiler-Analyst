d96b011fcbbc7263fe69abbf7e51f6b0f566144c
afraser <afraser@1fc53939-2000-0410-845c-e8453a809027>
2009-10-08 14:35:50 +0000

    Scatterplot!
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CPAnalyst@8145 1fc53939-2000-0410-845c-e8453a809027

--- src/PlotPanel.py
+++ src/PlotPanel.py
@@ -1,68 +1,157 @@
-from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
-from matplotlib.figure import Figure
-import matplotlib.cm
+#!/usr/bin/env python
+
+import matplotlib
+matplotlib.interactive(True)
+matplotlib.use('WXAgg')
+
+import numpy as num
 import wx
-import numpy as np
 
+class PlotPanel (wx.Panel):
+    '''
+    The PlotPanel has a Figure and a Canvas. OnSize events simply set a 
+    flag, and the actual resizing of the figure is triggered by an Idle event.
+    '''
+    def __init__(self, parent, color=None, dpi=None, **kwargs):
+        from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
+        from matplotlib.figure import Figure
 
-#
-#
-# THIS MODULE IS NOT WORKING
-#
-#
-
-class PlotPanel(wx.Panel):
-    def __init__( self, parent, **kwargs ):
+        # initialize Panel
+        if 'id' not in kwargs.keys():
+            kwargs['id'] = wx.ID_ANY
+        if 'style' not in kwargs.keys():
+            kwargs['style'] = wx.NO_FULL_REPAINT_ON_RESIZE
         wx.Panel.__init__(self, parent, **kwargs)
-        
-        self.parent = parent
-        self.im = None
-        self.figure = Figure( None, None )
-        self.canvas = FigureCanvasWxAgg( self, -1, self.figure )
-        
-        self.Bind(wx.EVT_PAINT, self.OnPaint)
-        self.Bind(wx.EVT_SIZE, self.OnSize)
-        
-        
-    def SetMap(self, map):
-        self.map = matplotlib.cm.get_cmap(map)
-        if self.im is not None:
-            self.im.set_cmap(self.map)
-            self.Refresh()
 
+
+        # initialize matplotlib stuff
+        self.figure = Figure(None, dpi)
+        self.canvas = FigureCanvasWxAgg(self, -1, self.figure)
+        self.SetColor(color)
+
+        self.draw()
+
+        self._resizeflag = False
+
+        self.Bind(wx.EVT_IDLE, self._onIdle)
+        self.Bind(wx.EVT_SIZE, self._onSize)
+
+    def SetColor(self, rgbtuple=None):
+        """Set figure and canvas colours to be the same."""
+        if rgbtuple is None:
+            rgbtuple = wx.SystemSettings.GetColour(wx.SYS_COLOUR_BTNFACE).Get()
+        clr = [c / 255. for c in rgbtuple]
+        self.figure.set_facecolor(clr)
+        self.figure.set_edgecolor(clr)
+        self.canvas.SetBackgroundColour(wx.Colour(*rgbtuple))
+
+    def _onSize(self, event):
+        self._resizeflag = True
+
+    def _onIdle(self, evt):
+        if self._resizeflag:
+            self._resizeflag = False
+            self._SetSize()
+
+    def _SetSize(self):
+        pixels = self.GetClientSize()
+        if 0 in pixels:
+            return
+        self.canvas.SetSize(pixels)
+        self.figure.set_size_inches(float(pixels[0]) / self.figure.get_dpi(),
+                                     float(pixels[1]) / self.figure.get_dpi())
+
+    def draw(self):
+        # abstract, to be overridden by child classes 
+        pass 
+
+
+class FigurePanel(PlotPanel):
+    def __init__(self, parent, point_lists, clr_list, **kwargs):
+        self.parent = parent
+        self.point_lists = point_lists
+        self.clr_list = clr_list
+
+        # initiate plotter
+        PlotPanel.__init__(self, parent, **kwargs)
+        self.SetColor((255, 255, 255))
     
-    def OnPaint(self, evt):
+    def setpointslists(self, points):
+        self.point_lists = points
+    
+    def getpointslists(self):
+        return self.point_lists
+    
+    def draw(self):
+        #Draw data.
         if not hasattr(self, 'subplot'):
             self.subplot = self.figure.add_subplot(111)
-        
-#        if self.im is None:
-#            a = np.arange(200.)
-#            a = np.vstack([a for x in range(20)])
-#            self.im = self.subplot.imshow(a, cmap=self.map, aspect='equal', extent=(0,self.Size[0],self.Size[1],0))
-#            self.subplot.axis('tight')
-#            self.subplot.axis('off')
-#            self.subplot.yaxis.set_visible(False)
-#            self.figure.subplots_adjust(0.,0.,1.,1.)
-#            self.canvas.SetSize(self.Size)
-#            self.figure.set_size_inches(6.,0.5)
-        
+
+        self.subplot.clear()
+
+        for i, pt_list in enumerate(self.point_lists):
+            plot_pts = num.array(pt_list)
+            clr = [float(c) / 255. for c in self.clr_list[i]]
+            self.subplot.scatter(plot_pts[:, 0], plot_pts[:, 1], color=clr)
+#            self.subplot.hist(plot_pts[:,0])
+#            self.subplot.plot( plot_pts[:,0], plot_pts[:,1], color=clr )
         self.canvas.draw()
-    
-    def OnSize(self, evt):
-        self.canvas.SetSize(self.Size)
-        self.figure.set_size_inches(float(self.Size[0]) / self.figure.get_dpi(),
-                                    float(self.Size[1]) / self.figure.get_dpi())
-        evt.Skip()
-        
-        
-        
-    
-            
-if __name__ == "__main__":
-    app = wx.PySimpleApp()
-    
-    frame = wx.Frame( None, wx.ID_ANY, 'WxPython and Matplotlib')#, size=(300,300) )
-    panel = PlotPanel(frame, size=(600,14))
+
+
+
+
+
+if __name__ == '__main__':
+    class DemoPlotPanel (PlotPanel):
+        """Plots several lines in distinct colors."""
+        def __init__( self, parent, point_lists, clr_list, **kwargs ):
+            self.parent = parent
+            self.point_lists = point_lists
+            self.clr_list = clr_list
+
+            # initiate plotter
+            PlotPanel.__init__( self, parent, **kwargs )
+            self.SetColor( (255,255,255) )
+            self.Bind(wx.EVT_SIZE, self._onsize)
+
+        def draw( self ):
+            """Draw data."""
+            if not hasattr( self, 'subplot' ):
+                self.subplot = self.figure.add_subplot( 111 )
+
+            for i, pt_list in enumerate( self.point_lists ):
+                plot_pts = num.array( pt_list )
+                clr = [float( c )/255. for c in self.clr_list[i]]
+                self.subplot.plot( plot_pts[:,0], plot_pts[:,1], color=clr )
+                
+        def _onsize(self, evt):
+            self._SetSize()
+
+    theta = num.arange( 0, 45*2*num.pi, 0.02 )
+
+    rad0 = (0.8*theta/(2*num.pi) + 1)
+    r0 = rad0*(8 + num.sin( theta*7 + rad0/1.8 ))
+    x0 = r0*num.cos( theta )
+    y0 = r0*num.sin( theta )
+
+    rad1 = (0.8*theta/(2*num.pi) + 1)
+    r1 = rad1*(6 + num.sin( theta*7 + rad1/1.9 ))
+    x1 = r1*num.cos( theta )
+    y1 = r1*num.sin( theta )
+
+    points = [[(xi,yi) for xi,yi in zip( x0, y0 )],
+              [(xi,yi) for xi,yi in zip( x1, y1 )]]
+    clrs = [[225,200,160], [219,112,147]]
+
+    app = wx.PySimpleApp( 0 )
+    frame = wx.Frame( None, wx.ID_ANY, 'WxPython and Matplotlib', size=(300,300) )
+    panel = DemoPlotPanel( frame, points, clrs )
+#    panel = wx.Panel(frame, -1)
+#    panel.SetBackgroundColour('red')
+    btn = wx.Button(frame, -1, 'asdf')
+    sizer = wx.BoxSizer(wx.HORIZONTAL)
+    sizer.Add(panel, 1, wx.EXPAND)
+    sizer.Add(btn, 0)
+    frame.SetSizer(sizer)
     frame.Show()
-    
     app.MainLoop()
---
+++ src/Scatter.py
@@ -0,0 +1,161 @@
+from ColorBarPanel import ColorBarPanel
+from DBConnect import DBConnect, UniqueImageClause, image_key_columns
+from PlateMapPanel import *
+from PlotPanel import *
+from Properties import Properties
+import ImageTools
+import numpy as np
+import os
+import re
+import wx
+
+
+p = Properties.getInstance()
+db = DBConnect.getInstance()
+
+
+class DataSourcePanel(wx.Panel):
+    '''
+    A panel with controls for selecting the source data for a scatterplot 
+    '''
+    def __init__(self, parent, figpanel, **kwargs):
+        wx.Panel.__init__(self, parent, **kwargs)
+        
+        # the panel to draw charts on
+        self.figpanel = figpanel
+        
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.table_choice = wx.Choice(self, -1, choices=db.GetTableNames())
+        self.x_choice = wx.Choice(self, -1)
+        self.y_choice = wx.Choice(self, -1)
+        self.update_chart_btn = wx.Button(self, -1, "Update Chart")
+        
+        self.update_column_fields()
+        
+        sz = wx.BoxSizer(wx.HORIZONTAL)
+        sz.Add(wx.StaticText(self, -1, "table:"))
+        sz.AddSpacer((5,-1))
+        sz.Add(self.table_choice, 1, wx.EXPAND)
+        sizer.Add(sz)
+        sizer.AddSpacer((-1,5))
+        sz = wx.BoxSizer(wx.HORIZONTAL)
+        sz.Add(wx.StaticText(self, -1, "x-axis:"))
+        sz.AddSpacer((5,-1))
+        sz.Add(self.x_choice)
+        sizer.Add(sz)
+        sizer.AddSpacer((-1,5))
+        sz = wx.BoxSizer(wx.HORIZONTAL)
+        sz.Add(wx.StaticText(self, -1, "y-axis:"))
+        sz.AddSpacer((5,-1))
+        sz.Add(self.y_choice)
+        sizer.Add(sz)
+        sizer.AddSpacer((-1,5))
+        sizer.Add(self.update_chart_btn)    
+        
+        wx.EVT_CHOICE(self.table_choice, -1, self.on_table_selected)
+        wx.EVT_BUTTON(self.update_chart_btn, -1, self.on_update_pressed)   
+        self.Bind(wx.EVT_SIZE, self._onsize)
+        
+        self.SetSizer(sizer)
+        self.SetAutoLayout(1)
+        sizer.Fit(self)
+        self.Show(1)
+
+    def on_table_selected(self, evt):
+        self.update_column_fields()
+        
+    def update_column_fields(self):
+        tablename = self.table_choice.GetStringSelection()
+        fieldnames = db.GetColumnNames(tablename)
+        self.x_choice.Clear()
+        self.x_choice.AppendItems(fieldnames)
+        self.x_choice.SetSelection(0)
+        self.y_choice.Clear()
+        self.y_choice.AppendItems(fieldnames)
+        self.y_choice.SetSelection(0)        
+        
+    def on_update_pressed(self, evt):        
+        points = self.loadpoints(self.table_choice.GetStringSelection(),
+                                 self.x_choice.GetStringSelection(),
+                                 self.y_choice.GetStringSelection())
+        self.plotpoints(points)
+        
+    def removefromchart(self, event):
+        selected = self.plotfieldslistbox.GetSelection()
+        self.plotfieldslistbox.Delete(selected)
+        
+    def loadpoints(self, tablename, xpoints, ypoints):
+        #loads points from the database
+        n_points = 99999999
+        points = db.execute('SELECT %s, %s FROM %s LIMIT %s'%(xpoints, ypoints, tablename, n_points)) 
+        return [points]
+    
+    def plotpoints(self, points):
+        self.figpanel.setpointslists(points)
+        self.figpanel.draw()
+        
+    def _onsize(self, evt):
+        self.figpanel._SetSize()
+        evt.Skip()
+        
+
+
+
+def LoadProperties():
+    import os
+    dlg = wx.FileDialog(None, "Select a the file containing your properties.", style=wx.OPEN|wx.FD_CHANGE_DIR)
+    if dlg.ShowModal() == wx.ID_OK:
+        filename = dlg.GetPath()
+        os.chdir(os.path.split(filename)[0])  # wx.FD_CHANGE_DIR doesn't seem to work in the FileDialog, so I do it explicitly
+        p.LoadFile(filename)
+    else:
+        print 'Scatterplot requires a properties file.  Exiting.'
+        exit()
+
+            
+if __name__ == "__main__":
+    app = wx.PySimpleApp()
+        
+    # Load a properties file if passed in args
+    if len(sys.argv) > 1:
+        propsFile = sys.argv[1]
+        p.LoadFile(propsFile)
+    else:
+        LoadProperties()
+
+    points = [[(1, 1)],[(2, 2)],[(3, 3)],[(4, 4)],[(5, 5)]]
+    clrs = [[225, 200, 160], [219, 112, 147], [219, 112, 147], [219, 112, 147], [219, 112, 147]]
+    frame = wx.Frame(None, -1, "Scatter Plot")
+    figpanel = FigurePanel(frame, points, clrs)
+    configpanel = DataSourcePanel(frame, figpanel)
+    
+    sizer = wx.BoxSizer(wx.VERTICAL)
+    sizer.Add(figpanel, 1, wx.EXPAND)
+    sizer.Add(configpanel, 0)
+    frame.SetSizer(sizer)
+    
+    frame.Show(1)
+    app.MainLoop()
+
+#if __name__ == "__main__":
+#    points = [[(1, 1)],[(2, 2)],[(3, 3)],[(4, 4)],[(5, 5)]]
+#    clrs = [[225, 200, 160], [219, 112, 147], [219, 112, 147], [219, 112, 147], [219, 112, 147]]
+#
+#    app = wx.PySimpleApp()
+#
+#    p.LoadFile('/Users/afraser/ExampleImages/cpa_example/example.properties')
+#    
+#    frame = wx.Frame(None, -1, "Scatter Plot")
+#    figpanel = FigurePanel(frame, points, clrs)
+#    configpanel = DataSourcePanel(frame, figpanel)
+#    
+##    sizer = wx.BoxSizer(wx.VERTICAL)
+##    sizer.Add(figpanel, 1, wx.EXPAND)
+##    sizer.Add(configpanel, 0)
+#    
+##    frame.SetSizer(sizer)
+#    
+#    frame.Show(1)
+#    app.MainLoop()
+
