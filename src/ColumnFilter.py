9599d397b819054afaa46bfd5d69a833cfc51f1d
afraser <afraser@1fc53939-2000-0410-845c-e8453a809027>
2009-12-16 22:08:09 +0000

    Construct to build filters on the fly.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CPAnalyst@8725 1fc53939-2000-0410-845c-e8453a809027

--- src/ColumnFilter.py
+++ src/ColumnFilter.py
@@ -1,7 +1,9 @@
 import sys
 import wx
+from Filter import Filter
 from Properties import Properties
 from DBConnect import DBConnect
+from wx.combo import OwnerDrawnComboBox as ComboBox
 
 
 p = Properties.getInstance()
@@ -16,68 +18,118 @@ class ColumnFilterPanel(wx.Panel):
     Experimental.  Creates a UI that allows the user to create WHERE clauses
     by selecting 1) a DB column name, 2) a comparator, and 3) a value
     '''
-    def __init__(self, parent, **kwargs):
+    def __init__(self, parent, tables, **kwargs):
         wx.Panel.__init__(self, parent, **kwargs)
         
         self.fieldSets = []
-        
-        self.cols = db.GetColumnNames(p.image_table)
-        self.types = db.GetColumnTypes(p.image_table)
-        colChoice = wx.Choice(self, choices=self.cols, size=(150,-1))
-        #colChoice.Select(0)
-        comparatorChoice = wx.Choice(self, choices=self.GetComparatorsForType(self.types[0]))
-        valueField = wx.ComboBox(self, -1, value='', choices=self.GetValueChoicesForColumnIdx(colChoice.GetSelection()))
-        
-        self.okBtn = wx.Button(self, -1, 'OK')
-        self.fieldSets += [[colChoice, comparatorChoice, valueField]]
+        self.tables = tables
+        self.types = {}
+        self.types[p.image_table] = db.GetColumnTypes(p.image_table)
+        self.tableChoice = ComboBox(self, choices=self.tables, size=(150,-1))
+        self.tableChoice.Select(0)
+        self.colChoice = ComboBox(self, choices=db.GetColumnNames(p.image_table), size=(150,-1), style=wx.CB_READONLY)
+        self.comparatorChoice = ComboBox(self)
+        self.update_comparator_choice()
+        self.valueField = wx.ComboBox(self, -1, value='')
         
         colSizer = wx.BoxSizer(wx.HORIZONTAL)
-        colSizer.Add(colChoice)
-        colSizer.AddSpacer((5,20))
-        colSizer.Add(comparatorChoice)
-        colSizer.AddSpacer((5,20))
-        colSizer.Add(valueField)
+        colSizer.Add(self.tableChoice, 1, wx.EXPAND)
+        colSizer.AddSpacer((5,-1))
+        colSizer.Add(self.colChoice, 1, wx.EXPAND)
+        colSizer.AddSpacer((5,-1))
+        colSizer.Add(self.comparatorChoice, 0.5, wx.EXPAND)
+        colSizer.AddSpacer((5,-1))
+        colSizer.Add(self.valueField, 1, wx.EXPAND)
         
-        rowSizer = wx.BoxSizer(wx.VERTICAL)
-        rowSizer.Add(colSizer)
-        rowSizer.Add(self.okBtn)
+        self.SetSizer(colSizer)
+        self.tableChoice.Bind(wx.EVT_COMBOBOX, self.on_select_table)
+        self.colChoice.Bind(wx.EVT_COMBOBOX, self.on_select_col)
         
-        self.SetSizer(rowSizer)
-        colChoice.Bind(wx.EVT_CHOICE, self.OnSelectColumn)
+    def on_select_col(self, evt):
+        self.update_comparator_choice()
+        self.update_value_choice()
         
-    def GetComparatorsForType(self, colType):
-        if colType == str:
-            return ['=', '!=', 'REGEXP']
-        if colType == int or colType == float or colType == long:
-            return ['=', '!=', '<', '>', '<=', '>=']
-        return []
+    def on_select_table(self, evt):
+        self.update_col_choice()
+        self.update_comparator_choice()
+        self.update_value_choice()
+        
+    def update_col_choice(self):
+        table = self.tableChoice.GetStringSelection()
+        self.colChoice.SetItems(db.GetColumnNames(table))
+        self.colChoice.Select(0)
+        
+    def update_comparator_choice(self):
+        table = self.tableChoice.GetStringSelection()
+        colidx = self.colChoice.GetSelection()
+        coltype = db.GetColumnTypes(table)[colidx]
+        print coltype
+        comparators = []
+        if coltype in [str, unicode]:
+            comparators = ['=', '!=', 'REGEXP']
+        if coltype in [int, float, long]:
+            comparators = ['=', '!=', '<', '>', '<=', '>=']
+        self.comparatorChoice.SetItems(comparators)
+        self.comparatorChoice.Select(0)
+        
+    def update_value_choice(self):
+        table = self.tableChoice.GetStringSelection()
+        column = self.colChoice.GetStringSelection()
+        colidx = self.colChoice.GetSelection()
+        coltype = db.GetColumnTypes(table)[colidx]
+        vals = []
+        if coltype == str:# or coltype == int or coltype == long:
+            res = db.execute('SELECT DISTINCT %s FROM %s ORDER BY %s'%(column, table, column))
+            vals = [str(row[0]) for row in res]
+        self.valueField.SetItems(vals)
     
-    def GetValueChoicesForColumnIdx(self, index):
-        column = self.cols[index]
-        colType = self.types[index]
-        res = db.execute('SELECT %s FROM %s GROUP BY %s'%(column, p.image_table, column))
-        if colType == str or colType == int or colType == long:
-            return [str(row[0]) for row in res]
-        if colType == float:
-            return []
+    def get_filter(self):
+        table = self.tableChoice.GetStringSelection()
+        column = self.colChoice.GetStringSelection()
+        comparator = self.comparatorChoice.GetStringSelection()
+        value = self.valueField.GetValue()
+        return Filter(table, column, comparator, value)
         
-    def OnSelectColumn(self, evt):
-        evtObj = evt.GetEventObject()
-        for fieldSet in self.fieldSets:
-            if fieldSet[0] == evtObj:
-                selIdx = fieldSet[0].GetSelection()
-                fieldSet[1].SetItems(self.GetComparatorsForType(self.types[selIdx]))
-                fieldSet[2].SetItems(self.GetValueChoicesForColumnIdx(fieldSet[0].GetSelection()))
                 
-
 class ColumnFilterFrame(wx.Frame):
-    def __init__(self, parent, **kwargs):
+    def __init__(self, parent, tables, **kwargs):
         wx.Frame.__init__(self, parent, -1, **kwargs)
-        self.panel = ColumnFilterPanel(self, **kwargs)
+        self.tables = tables
+        self.panels = [ColumnFilterPanel(self, tables)]
+        self.addbtn = wx.Button(self, -1, 'Add Column')
+        self.ok = wx.Button(self, -1, 'OK')
+        self.cancel = wx.Button(self, -1, 'Cancel')
+        
+        vsizer = wx.BoxSizer(wx.VERTICAL)
+        hsizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.SetSizer(vsizer)
+        hsizer.Add(self.ok)
+        hsizer.Add(self.cancel)
+        vsizer.Add(self.panels[0])
+        vsizer.Add(self.addbtn)
+        vsizer.Add(hsizer)
+        
+        self.addbtn.Bind(wx.EVT_BUTTON, self.add_column)
+        self.ok.Bind(wx.EVT_BUTTON, lambda(x):self.EndModal(wx.OK))
+        self.cancel.Bind(wx.EVT_BUTTON, lambda(x):self.EndModal(wx.CANCEL))
+        
+    def get_filter(self):
+        filter = Filter()
+        for panel in self.panels:
+            filter += panel.get_filter()
+        return filter
     
+    def add_column(self, evt):
+        self.panels += [ColumnFilterPanel(self, self.tables)]
+        self.GetSizer().Insert(len(self.panels)-1, self.panels[-1])
+        self.Layout()
 
     
 if __name__ == "__main__":
+    
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+    
     app = wx.PySimpleApp()
     
     # Load a properties file if passed in args
@@ -85,11 +137,13 @@ if __name__ == "__main__":
         propsFile = sys.argv[1]
         p.LoadFile(propsFile)
     else:
-        p.LoadFile('../properties/nirht_test.properties')
+        p.LoadFile('/Users/afraser/Desktop/cpa_example/example.properties')
         #p.LoadFile('../properties/Gilliland_LeukemiaScreens_Validation.properties')
 
 
-    cff = ColumnFilterFrame(None)
-    cff.Show()
+    cff = ColumnFilterFrame(None, tables=[p.image_table], size=(550,-1))
+    if cff.Show(True)==wx.OK:
+        print cff.get_filter()
+    cff.Destroy()
 
     app.MainLoop()
---
+++ src/Filter.py
@@ -0,0 +1,99 @@
+from DBConnect import *
+from Properties import Properties
+
+p = Properties.getInstance() 
+
+def merge_filters(filters):
+    ''' Takes a list of filters, returns a single filter with all the where 
+    clauses combined'''
+    wc = []
+    for filter in filters:
+        wc += filter.get_where_clauses()
+    return Filter()
+
+class Filter:
+    '''
+    Represents a method for filtering results from the database.
+    Filters are comprised of one or more WhereClause classes.
+    '''
+    def __init__(self, table=None, column=None, comparator=None, value=None):
+        self.where_clauses = set([])
+        if None not in [table, column, comparator, value]:
+            self.where_clauses = [WhereClause(table, column, comparator, value)]
+        elif not table==column==comparator==value==None:
+            raise Exception, 'All Filter fields are required.'
+    
+    def __str__(self):
+        tables = set([cl.table for cl in self.where_clauses])
+        # currently expect table to be the per image table
+        return ('SELECT %s FROM %s WHERE %s'%(UniqueImageClause(p.image_table), 
+                ', '.join(tables),
+                ' AND '.join(map(str, self.where_clauses))))
+    
+    def get_where_clauses(self):
+        return self.where_clauses
+    
+    def add_column(self, table, column, comparator, value):
+        self.where_clauses += [WhereClause(table, column, comparator, value)]
+        
+    def add_where_clause(self, wc):
+        self.where_clauses.add(wc)
+        
+    def add_filter(self, filter):
+        self.where_clauses.add(filter.get_where_clauses())
+        
+    def __add__(self, filter):
+        if isinstance(filter, Filter):
+            copy = Filter()
+            copy.where_clauses = set(self.where_clauses).union(filter.get_where_clauses())
+            return copy
+        else:
+            raise Exception, 'Cannot add type %s to a Filter'%(type(filter))
+
+
+class WhereClause:
+    def __init__(self, table, column, comparator, value):
+        self.table = table
+        self.column = column
+        self.comparator = comparator
+        self.value = value
+    
+    def __eq__(self, wc):
+        return (self.table==wc.table and 
+                self.column==wc.column and
+                self.comparator==wc.comparator and
+                self.value==wc.value)
+        
+    def __ne__(self, wc):
+        return not self.__eq__(wc)
+    
+    def __hash__(self):
+        return str(self).__hash__()
+        
+    def __str__(self):
+        return '%s.%s %s "%s"'%(self.table, self.column, self.comparator, self.value)
+    
+    
+    
+if __name__ == "__main__":
+    p.image_table = 'imtbl'
+    p.image_id = 'imnum'
+    
+    f = Filter('A','a','=','1')
+    f.add_column('C', 'c', '=', '1')
+    print f
+    f.add_column('C', 'cc', '=', '3')
+    print f
+    
+    f2 = Filter('A','a','=','1')
+    f3 = Filter('B','b','=','1')
+    
+    g = f + f2
+    g = g + f3
+    print g
+     
+    assert WhereClause('a','a','=','1') == WhereClause('a','a','=','1') 
+    assert not(WhereClause('a','a','=','1') != WhereClause('a','a','=','1')) 
+    assert not(WhereClause('a','a','=','1') == WhereClause('b','a','=','1')) 
+    assert WhereClause('a','a','=','1') != WhereClause('b','a','=','1') 
+     
