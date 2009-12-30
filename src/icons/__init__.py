58ef10415254bd078a2dbb40fd8b9ed6b8fc6e2b
thouis <thouis@1fc53939-2000-0410-845c-e8453a809027>
2009-12-30 02:09:11 +0000

    more comments
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CPAnalyst@8923 1fc53939-2000-0410-845c-e8453a809027

--- src/icons/__init__.py
+++ src/icons/__init__.py
@@ -3,16 +3,22 @@ import os.path
 import glob
 import sys
 
-# py2exe puts this module into its library.zip, and I'm not sure how
-# to stop it from doing so.  However, I can force it to include the
-# icons in a directory at the top level.
+
+# This bit of code provides simple importing of images, as instances
+# of wx.Image.  An image in a file named ABC.png copied into the icons
+# directory can be imported as icons.ABC.
+
 if 'library.zip' in __path__[0]:
+    # py2exe puts this module into its library.zip, and I'm not sure
+    # how to stop it from doing so.  However, I can force it to
+    # include the icons in a directory at the top level.
     search_path = 'icons'
 else:
     search_path = __path__[0]
 
 for f in glob.glob(os.path.join(search_path, "*.png")):
     globals()[os.path.basename(f)[:-4]] = wx.Image(f)
+
 
 def get_cpa_icon(size=None):
     '''The CellProfiler Analyst icon as a wx.Icon'''
