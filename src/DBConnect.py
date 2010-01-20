eb4239a45797de419efd32f721c3f1c8d11f8066
afraser <afraser@1fc53939-2000-0410-845c-e8453a809027>
2010-01-20 15:35:59 +0000

    oops!
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CPAnalyst@9099 1fc53939-2000-0410-845c-e8453a809027

--- src/DBConnect.py
+++ src/DBConnect.py
@@ -889,7 +889,7 @@ class DBConnect(Singleton):
         for q in create_stmts:
             self.execute(q)
         
-        if self.gui_parent is not None and issubclass(gui_parent.__class__, wx.Window):
+        if self.gui_parent is not None and issubclass(self.gui_parent.__class__, wx.Window):
             import wx
             dlg = wx.ProgressDialog('Creating sqlite DB...', '0% Complete', 100, self.gui_parent, wx.PD_ELAPSED_TIME | wx.PD_ESTIMATED_TIME | wx.PD_REMAINING_TIME | wx.PD_CAN_ABORT)
         else:
@@ -1347,8 +1347,8 @@ if __name__ == "__main__":
 
 #    p.LoadFile('../properties/Gilliland_LeukemiaScreens_Validation.properties')
 #    p.LoadFile('../properties/nirht_test.properties')
-    p.LoadFile('../test_data/export_to_db_test.properties')
-#    p.LoadFile('../test_data/nirht_local.properties')
+#    p.LoadFile('../test_data/export_to_db_test.properties')
+    p.LoadFile('../test_data/nirht_local.properties')
     
     dm.PopulateModel()
 
