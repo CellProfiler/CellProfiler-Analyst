5cd541a2c5b11be8cf9c96ae06e61fa170a5d886
afraser <afraser@1fc53939-2000-0410-845c-e8453a809027>
2010-10-27 21:26:10 +0000

    bugfix: Need to handle grayscale images with 3+ channels properly after bioformats returns them. Also need to cross check the user settings in channels_per_image when loading color images.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CPAnalyst@10646 1fc53939-2000-0410-845c-e8453a809027

--- src/imagereader.py
+++ src/imagereader.py
@@ -12,17 +12,34 @@ class ImageReader(object):
         returns a list of channels as numpy float32 arrays
         '''
         channels = []
-        for fd in fds:
+        for i, fd in enumerate(fds):
             if p.image_url_prepend and p.image_url_prepend.lower().startswith('http://'):
                 url = 'http://' + urllib2.quote(p.image_url_prepend[7:]) + urllib2.quote(fd)
             else:
                 url = fd
+            print 'loading image from ', url
             lip = LoadImagesImageProvider("dummy", "", url, True)
             image = lip.provide_image(None).pixel_data
-            if image.ndim == 2:
-                channels += [image]
-            elif image.ndim >= 3:
+            
+            if p.channels_per_image[i] == '1':
+                if image.ndim == 2:
+                    channels += [image]
+                else:
+                    channels += [image[:,:,0]]
+            elif p.channels_per_image[i] == '3':
+                channels += [image[:,:,i] for i in range(3)]
+            elif p.channels_per_image[i] >= '4':
+                if image.shape[2] != p.channels_per_image[i]:
+                    raise ('Your properties file specifies %s channels for '
+                           'image #%s but %s channels were found. Make sure '
+                           '"channels_per_image" is set correctly in your '
+                           'properties file.'%(p.channels_per_image[i], i+1, 
+                                               image.shape[2]))
                 channels += [image[:,:,i] for i in range(image.shape[2])]
+            else:
+                raise ('Invalid number of channels (%s) specified for image #%s.'
+                       ' Make sure "channels_per_image" is set correctly in your'
+                       'properties file.'%(p.channels_per_image[0] ,i+1))
         
         from imagetools import check_image_shape_compatibility
         check_image_shape_compatibility(channels)
