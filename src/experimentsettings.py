623d5c5a482806809f9fc923808108708ab7551c
afraser <afraser@1fc53939-2000-0410-845c-e8453a809027>
2010-11-18 21:22:04 +0000

    added methods to get field tags matching a prefix and instance and to remove fields.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CPAnalyst@10691 1fc53939-2000-0410-845c-e8453a809027

--- src/experimentsettings.py
+++ src/experimentsettings.py
@@ -11,12 +11,22 @@ class ExperimentSettings(Singleton):
     def get_field(self, tag, default=None):
         return self.global_settings.get(tag, default)
     
+    def remove_field(self, tag):
+        if self.get_field(tag) is not None:
+            self.global_settings.pop(tag)
+    
     def get_field_instances(self, tag_prefix):
         '''returns a list of unique instance ids for each tag beginning with 
         tag_prefix'''
         ids = set([tag.rsplit('|', 1)[-1] for tag in self.global_settings
                    if tag.startswith(tag_prefix)])
         return list(ids)
+
+    def get_field_tags(self, tag_prefix, instance=''):
+        '''returns a list of all tags beginning with tag_prefix. If instance
+        is passed in, only tags of the given instance will be returned'''
+        return [tag for tag in self.global_settings 
+                if tag.startswith(tag_prefix) and tag.endswith(instance)]
     
     def clear(self):
         self.global_settings = {}
@@ -26,7 +36,6 @@ class ExperimentSettings(Singleton):
         f = open(file, 'w')
         for field, value in self.global_settings.items():
             f.write('%s = %s\n'%(field, repr(value)))
-        #self.timeline.save_to_file(f)
         f.close()
 
     def load_from_file(self, file):
@@ -37,23 +46,3 @@ class ExperimentSettings(Singleton):
             field = field.strip()
             self.set_field(field, eval(value))
         f.close()
-        
-        
-        
-if __name__ == "__main__":
-    settings = ExperimentSettings.getInstance()
-    settings.set_field('test_string', 'test_value')
-    settings.set_field('test_list',   ['data', 3])
-    settings.set_field('test_int',    2)
-    settings.set_field('test_float',  234.3)
-    
-    settings.save_to_file('/Users/afraser/Desktop/test.txt')
-    print settings.global_settings.items()
-    before = settings.global_settings.items()
-    
-    settings.load_from_file('/Users/afraser/Desktop/test.txt')    
-    print settings.global_settings.items()
-    after = settings.global_settings.items()
-    
-    for a, b in zip(sorted(before), sorted(after)):
-        assert a == b, 'loaded data is not the same as the saved data.'
