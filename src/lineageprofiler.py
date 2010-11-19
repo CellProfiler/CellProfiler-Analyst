4c7b84cad0f3eb21b76c6717f3d518e2a62a15eb
afraser <afraser@1fc53939-2000-0410-845c-e8453a809027>
2010-11-19 23:07:05 +0000

    modified Timeline and Event code to require well_ids in the form (plate_id, well_id).
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CPAnalyst@10704 1fc53939-2000-0410-845c-e8453a809027

--- src/lineageprofiler.py
+++ src/lineageprofiler.py
@@ -51,14 +51,16 @@ class Timeline(object):
       self.stock = stock
       # events : a chronologically ordered list of events
       self.events = []
-      # plates : a list of plates included in the experiment
-      #         (all events will take place on these plates)
-      self.plates = []
+      # plate_ids : a list of plate ids included in the experiment
+      #             (all events will take place on these plates)
+      self.plate_ids = set([])
       
-   def add_event(self, timepoint, action, plate_id, well_ids):
+   def add_event(self, timepoint, action, well_ids):
       '''Creates and inserts an event into the timeline.
       '''
-      evt = Event(timepoint, action, plate_id, well_ids)
+      evt = Event(timepoint, action, well_ids)
+      print set([well_id[0] for well_id in well_ids])
+      self.plate_ids.update(set([well_id[0] for well_id in well_ids]))
       if len(self.events) == 0:
          self.events = [evt]
       else:
@@ -83,36 +85,36 @@ class Timeline(object):
       return list(set(well_ids))
    
    # CURRENTLY UNUSED
-##   def get_event_permutations(self, timepoint):
-##      '''returns a list of unique permutations of events at a given timepoint.
-##      Each permutation is a tuple of events that occurred.
-##      '''
-##      d = {}
-##      for event in self.get_events_at_timepoint(timepoint):
-##         # build a hash, mapping well_ids to lists of events that occurred
-##         for well in PlateDesign.get_well_ids(PlateDesign.get_plate_format(event.get_plate_id())):
-##            if well in event.get_well_ids():
-##               if d.get(well, None) is None:
-##                  d[well] = [event]
-##               else:
-##                  d[well] += [event]
-##      return set([tuple(set(d[k])) for k in d.keys()])
-   
-   def get_well_permutations(self, timepoint, plate_format):
-      '''returns a list of well-permutations
-      Each permutation is a tuple of wells that have a common state.
-      d = {(e1,)   :  (A01, A03),
-           (e1,e2) :  (A02, B03),
-           (e2, )  :  (B02)
-           (noop)  :  (B01)... }
+   def get_event_permutations(self, timepoint):
+      '''returns a list of unique permutations of events at a given timepoint.
+      Each permutation is a tuple of events that occurred.
       '''
       d = {}
-      for well in PlateDesign.get_well_ids(plate_format):
-         events_in_well = self.get_events_in_well(well, timepoint)
+      for event in self.get_events_at_timepoint(timepoint):
+         # build a hash, mapping well_ids to lists of events that occurred
+         for well in PlateDesign.get_well_ids(PlateDesign.get_plate_format(event.get_plate_id())):
+            if well in event.get_well_ids():
+               if d.get(well, None) is None:
+                  d[well] = [event]
+               else:
+                  d[well] += [event]
+      return set([tuple(set(d[k])) for k in d.keys()])
+   
+   def get_well_permutations(self, timepoint):
+      '''returns a list of well-permutations
+      Each permutation is a tuple of wells that have a common state.
+      d = {(e1,)   :  ((p1 A01), (p1, A03)),
+           (e1,e2) :  ((p1, A02), (p1, B03)),
+           (e2, )  :  ((p1, B02))
+           (noop)  :  ((p1, B01)) }
+      '''
+      d = {}
+      for wellid in self.get_all_well_ids():
+         events_in_well = self.get_events_in_well(wellid, timepoint)
          if d.get(events_in_well, None) is None:
-            d[events_in_well] = [well]
+            d[events_in_well] = [wellid]
          else:
-            d[events_in_well] += [well]
+            d[events_in_well] += [wellid]
       return set([tuple(set(d[k])) for k in d.keys()])
    
    def get_lineage_tree(self):
@@ -130,7 +132,7 @@ class Timeline(object):
          '''
          timepoint = self.get_unique_timepoints()[tp_idx]
          subwells = []
-         for wells in self.get_well_permutations(timepoint, P6):
+         for wells in self.get_well_permutations(timepoint):
             wellset = sorted(set(parent.get_well_ids()).intersection(wells))
             if len(wellset) > 0:
                subwells += [wellset]
@@ -157,7 +159,7 @@ class Timeline(object):
       return build_tree(root)
       
    def get_nodes_by_timepoint(self):
-      '''
+      '''returns a dict mapping timepoints to LineageNodes
       '''
       def get_dict(node, d):
          if not node.children:
@@ -179,10 +181,19 @@ class Timeline(object):
       '''
       return [evn for evn in self.events if (evn.get_timepoint() == timepoint)]
    
-   def get_events_in_well(self, well, timepoint):
+   def get_events_in_well(self, wellid, timepoint):
       '''returns a tuple of events that occurred in the given well at timepoint
       '''
-      return tuple([evt for evt in self.get_events_at_timepoint(timepoint) if well in evt.get_well_ids()])
+      return tuple([evt for evt in self.get_events_at_timepoint(timepoint) 
+                    if wellid in evt.get_well_ids()])
+   
+   def get_all_well_ids(self):
+      '''returns unique well ids from all plates associated with this timeline.
+      '''
+      pw_ids = []
+      for plate_id in self.plate_ids:
+         pw_ids += [(plate_id, well_id) for well_id in PlateDesign.get_well_ids(PlateDesign.get_plate_format(plate_id))]
+      return pw_ids
          
    def save(self, filename):
       '''Saves the timeline to filename
@@ -199,22 +210,17 @@ class Event(object):
    '''An Event is an action that was taken at some timepoint that is associated
    with a list of well_ids.
    '''
-   def __init__(self, timepoint, action, plate_id, well_ids):
+   def __init__(self, timepoint, action, well_ids):
       '''timepoint : the timepoint that this event occurred at
       action : the action that took place
-      plate_id : plate id
       wells_ids : the list of well_ids that this event effects
       '''
       self.action = action
-      self.plate = plate_id
       self.wells = well_ids
       self.timepoint = timepoint
 
    def get_timepoint(self):
       return self.timepoint
-   
-   def get_plate_id(self):
-      return self.plate
    
    def get_well_ids(self):
       return self.wells
@@ -273,70 +279,37 @@ class LineageNode(object):
 #
 if __name__ == '__main__':
    t = Timeline('U2OS')
+   PlateDesign.add_plate('plate1', P6)
+   PlateDesign.add_plate('plate2', P6)
+   all_wells = PlateDesign.get_well_ids(PlateDesign.get_plate_format('plate1'))
    
-   #PlateDesign.add_plate('fred', P6)
-   #all_wells = PlateDesign.get_well_ids(PlateDesign.get_plate_format('fred'))
+   t.add_event(1, 'seed', [('plate1', well) for well in all_wells]+[('plate2', well) for well in all_wells])
    
-   #t.add_event(1, 'seed', 'fred', all_wells)
-   
-   #t.add_event(2, 'treatment1', 'fred', ['A01', 'A02', 'A03', 'B03'])
-   #t.add_event(2, 'treatment2', 'fred', [ 'A02', 'B02', 'B03'])
+   t.add_event(2, 'treatment1', [('plate1', 'A01'), ('plate1', 'A02'), ('plate1', 'A03'), 
+                                                                       ('plate1', 'B03')])
+   t.add_event(2, 'treatment2', [                   ('plate1', 'A02'), 
+                                                    ('plate1', 'B02'), ('plate1', 'B03'), 
+                                 ('plate2', 'A01')])
    #untreated_wells = set(all_wells) - set(t.get_well_ids(2))
-   #t.add_event(2, NO_EVENT,     'fred', untreated_wells)
+   #t.add_event(2, NO_EVENT,     'plate1', untreated_wells)  
 
-   #t.add_event(3, 'treat', 'fred', ['A02'])
-   #t.add_event(3, 'wash', 'fred', ['A01'])
+   t.add_event(3, 'treat', [('plate1', 'A01'),                         ('plate1', 'A03'), 
+                                                  ('plate1', 'B02')])
+   t.add_event(3, 'wash', [                       ('plate1', 'B02'), ('plate1', 'B03')])
    #untreated_wells = set(all_wells) - set(t.get_well_ids(3))
-   #t.add_event(3, NO_EVENT, 'fred', untreated_wells)
+   #t.add_event(3, NO_EVENT, 'plate1', untreated_wells)
    
-   #t.add_event(4, 'imaging', 'fred', PlateDesign.get_well_ids(P6))
-   
-   
-   PlateDesign.add_plate('fred', P6)
-   all_wells = PlateDesign.get_well_ids(PlateDesign.get_plate_format('fred'))
-   
-   t.add_event(1, 'seed', 'fred', all_wells)
-   
-   t.add_event(2, 'treatment1', 'fred', ['A01', 'A02', 'A03', 'B03'])
-   t.add_event(2, 'treatment2', 'fred', [ 'A02', 'B02', 'B03'])
-   untreated_wells = set(all_wells) - set(t.get_well_ids(2))
-   t.add_event(2, NO_EVENT,     'fred', untreated_wells)  
+   t.add_event(4, 'spin', [('plate1', 'A01'), ('plate1', 'A02'), ('plate1', 'A03')])
+   #untreated_wells = set(all_wells) - set(t.get_well_ids(4))
+   #t.add_event(4, NO_EVENT, 'plate1', untreated_wells)
 
-   t.add_event(3, 'treat', 'fred', ['A01', 'A03', 'B02'])
-   t.add_event(3, 'wash', 'fred', ['B02', 'B03'])
-   untreated_wells = set(all_wells) - set(t.get_well_ids(3))
-   t.add_event(3, NO_EVENT, 'fred', untreated_wells)
-   
-   t.add_event(4, 'spin', 'fred', ['B01', 'A02', 'A03'])
-   untreated_wells = set(all_wells) - set(t.get_well_ids(4))
-   t.add_event(4, NO_EVENT, 'fred', untreated_wells)
-   
    d = t.get_nodes_by_timepoint()
    
    for time in d.keys():
       print str(time)+"\t"+str([node.id for node in d[time]])
       
-   #t.add_event(1, 'seed', 'fred', all_wells)
-   
-   #t.add_event(2, 'treatment1', 'fred', ['A01', 'A02', 'A03', 'B03'])
-   #t.add_event(2, 'treatment2', 'fred', [ 'A02', 'B02', 'B03'])
-   #untreated_wells = set(all_wells) - set(t.get_well_ids(2))
-   #t.add_event(2, NO_EVENT,     'fred', untreated_wells)
-
-   #t.add_event(3, 'treat', 'fred', ['A02'])
-   #t.add_event(3, 'wash', 'fred', ['A01'])
-   #untreated_wells = set(all_wells) - set(t.get_well_ids(3))
-   #t.add_event(3, NO_EVENT, 'fred', untreated_wells)
-   
-   #t.add_event(4, 'imaging', 'fred', PlateDesign.get_well_ids(P6))
-##   for p in t.get_well_permutations(2, P6 ):
-##      print [str(x) for x in p]
-
-##   for p in t.get_event_permutations(2):
-##      print [str(x) for x in p]
-
-##   for p in t.get_event_permutations(3):
-##      print [str(x) for x in p]
+   for p in t.get_well_permutations(2):
+      print [x for x in p]
 
    tree = t.get_lineage_tree()
    
@@ -347,7 +320,7 @@ if __name__ == '__main__':
    tcroot = tc.AddRoot("ROOT")
    def populate_wx_tree(wxparent, tnode):
       for child in tnode.children:    
-         subtree = tc.AppendItem(wxparent, ', '.join(child.get_well_ids()))
+         subtree = tc.AppendItem(wxparent, ', '.join([str(id) for id in child.get_well_ids()]))
          populate_wx_tree(subtree, child)
          tc.Expand(subtree)
    populate_wx_tree(tcroot, tree)    
