4139fbf093f45709f7532e4a58e1b7e2b617311c
afraser <afraser@1fc53939-2000-0410-845c-e8453a809027>
2010-12-09 02:26:24 +0000

    Handle failed tile fetching better
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CPAnalyst@10765 1fc53939-2000-0410-845c-e8453a809027

--- src/tilecollection.py
+++ src/tilecollection.py
@@ -125,15 +125,12 @@ class TileLoader(threading.Thread):
                 if not self.tile_collection.tileData.get(obKey, None):
                     continue
 
-                try:
-                    new_data = imagetools.FetchTile(obKey)
-                except Exception, e:
-                    try:
-                        wx.MessageDialog('ERROR FETCHING %s!: %s\n'%(p.object_name[0].upper(), e), 
-                                         'Error fetching %s'%(p.object_name)).ShowModal()
-                    except:
-                        logging.error('ERROR FETCHING %s!: %s\n'%(p.object_name[0].upper(), e))
+                # Get the tile
+                new_data = imagetools.FetchTile(obKey)
+                if new_data is None:
+                    #if fetching fails, leave the tile blank
                     continue
+                
                 tile_data = self.tile_collection.tileData.get(obKey, None)
                 
                 # Make sure tile hasn't been deleted outside this thread
