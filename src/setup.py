896087d576c71095480582d3e670c72c606462c5
afraser <afraser@1fc53939-2000-0410-845c-e8453a809027>
2011-06-03 14:53:23 +0000

    Fix cp icon error on showing x-validation plot. Fix CPA hang on shutdown.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CPAnalyst@11246 1fc53939-2000-0410-845c-e8453a809027

--- src/setup.py
+++ src/setup.py
@@ -3,27 +3,39 @@ import sys
 import os
 import os.path
 import numpy
-import glob
-
 # fix from
 #  http://mail.python.org/pipermail/pythonmac-sig/2008-June/020111.html
 import pytz
 pytz.zoneinfo = pytz.tzinfo
 pytz.zoneinfo.UTC = pytz.UTC
-
 import pilfix
 
+
+CP_HOME = '../../CellProfiler/'
+if not os.path.exists(CP_HOME):
+    raise Exception('CellProfiler source not found. Edit CP_HOME in setup.py')
+else:
+    sys.path.append(CP_HOME)
+
+#
+# Store version in cpa_version.py
+#
 if sys.platform == "darwin":
     os.system(''' svnversion | sed -e's/^/VERSION = \"/' -e 's/[0-9]*://' -e 's/M//' -e 's/$/\"/' > cpa_version.py ''')
 
 APPNAME = 'CPAnalyst'
 APP = ['cpa.py']
-DATA_FILES = []
+DATA_FILES = [('bioformats', [CP_HOME+'bioformats/loci_tools.jar']),
+              ('cellprofiler/icons', [CP_HOME+'cellprofiler/icons/CellProfilerIcon.png']), # needed for cpfigure used by classifier cross validation
+             ]
 OPTIONS = {'argv_emulation': True,
-           'iconfile': "icons/cpa.icns",
-           'packages': ['numpy', './icons'],
-           'excludes': ['nose', 'wx.tools'],
-           }
+           'iconfile' : "icons/cpa.icns",
+           'includes' : [ ],
+           'packages' : ['numpy', './icons', 'bioformats', 'killjavabridge', ],
+           'excludes' : ['nose', 'wx.tools', 'Cython', 'pylab', 'Tkinter',
+                         'scipy.weave', 'imagej'],
+           'resources' : [],
+          }
 
 setup(
     app=APP,
