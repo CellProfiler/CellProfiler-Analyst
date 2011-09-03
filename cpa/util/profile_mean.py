c0c46ee00cba9ae31f43573b686e984eaccb0018
auguste <auguste@1fc53939-2000-0410-845c-e8453a809027>
2011-09-03 02:46:33 +0000

    Added storage in txt file
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CPAnalyst@11473 1fc53939-2000-0410-845c-e8453a809027

--- cpa/util/profile_mean.py
+++ cpa/util/profile_mean.py
@@ -6,15 +6,11 @@ $ profile_mean.py properties_file cache_dir output_file [group [filter]]
 Writes a tab-delimited file containing the profiles. Example:
 
 		f1	f2
-Plate1	A01	0.123	4.567
-Plate1	A02	8.901	2.345
+GroupItem1	0.123	4.567
+GroupItem2	8.901	2.345
 
-or:
-
-	f1	f2
-Gene1	0.123	4.567
-Gene2	8.901	2.345
 """
+import io
 import sys
 import os
 import numpy as np
@@ -37,14 +33,11 @@ if __name__ == '__main__':
         #if len_argv == 6:
             #ffilter = sys.argv[5]
 
-            
-            
+    # test
     properties_file = 'C:\\Users\\auguste\\Documents\\CDRP\\CDP2\\CDP2.properties'
     cache_dir = 'Z:\\2008_12_04_Imaging_CDRP_for_MLPCN\\CDP2\\cache'
-    output_file = 'C:\\Users\\auguste\\Documents\\CDRP\\CDP2\\test'
+    output_file = 'C:\\Users\\auguste\\Documents\\CDRP\\CDP2\\test.txt'
     group = 'CompoundConcentration'
-    
-    #colnames = cache.get_colnames(cache_dir)
     
     cpa.properties.LoadFile(properties_file)
     
@@ -57,32 +50,43 @@ if __name__ == '__main__':
     plates = [row[1] for row in images_plates_wells]
     wells  = [row[2] for row in images_plates_wells]
     
-    # !! we have to include the whole well as soon as an image of a well is present in the group... not so great
+    colnames = cache.get_colnames(cache_dir)
+    
+    grps = '\t'.join("%s"%g for g in colnames_group)
+    cols = '\t'.join("%s"%c for c in colnames)
+    
+    text_file = open(output_file, "w")
+    text_file.write('%s\t%s\n' % (grps, cols))
+    
+    
+    # !! we have no choice but to include the whole well as soon as an image of a well is present in the group... not so great
     # This prevent the granularity to be below the well level and can lead to unexpected results in this case.
     # Solution: implement the cache at image level and not well level (hence drawback of increasing file access, also not really usefull in practice)
-    
-    for gp in mapping_group_images.keys()[0:3]:
-            # gp = mapping_group_images.keys()[0]            
-            plate_well = []
-            # ensure unicity of well
-            for image in mapping_group_images[gp]:
-                i = images.index(image[0])
-                value = [plates[i], wells[i]]
-                if not value in plate_well:
-                    plate_well.append(value)
-            # collect data
-            rawdata = []
-            for plate, well in plate_well:
-                plate_dir = os.path.join(cache_dir, '%s' % plate)
-                for row in cache.load(cache_dir, '%s' % plate, '%s' % well):
-                    rawdata.append(row)
-            
-            # compute the mean vector
-            normalizeddata = rawdata #percentiles.normalize(rowdata, cache_dir, plate)
-            normalizeddata_mean = np.mean(normalizeddata, axis = 0)
-            print normalizeddata_mean[0:5]
-            #add a line normalizeddata_mean in the file
+    for gp in mapping_group_images.keys():
+        plate_well = []
+        # ensure unicity of each well
+        for image in mapping_group_images[gp]:
+            i = images.index(image[0])
+            value = [plates[i], wells[i]]
+            if not value in plate_well:
+                plate_well.append(value)
+        # collect data
+        rawdata = []
+        for plate, well in plate_well:
+            plate_dir = os.path.join(cache_dir, '%s' % plate)
+            for row in cache.load(cache_dir, '%s' % plate, '%s' % well):
+                rawdata.append(row)
+        
+        # compute the mean vector
+        normalizeddata = rawdata #percentiles.normalize(rowdata, cache_dir, plate) # <= !!! Cannot normalized because percentiles aren't available
+        normalizeddata_mean = np.mean(normalizeddata, axis = 0)
+        
+        groupItem = '\t'.join("%s"%i for i in gp)
+        values = '\t'.join("%d"%v for v in normalizeddata_mean)
+        text_file.write('%s\t%s\n' % (groupItem, values))
+        #add a line normalizeddata_mean in the file
 
+    text_file.close()
 
     
     
