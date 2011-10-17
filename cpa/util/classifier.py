69343bb5d65ec069da73c76a3ea42c40966a5878
auguste <auguste@1fc53939-2000-0410-845c-e8453a809027>
2011-10-17 19:46:59 +0000

    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CPAnalyst@11601 1fc53939-2000-0410-845c-e8453a809027

--- cpa/util/classifier.py
+++ cpa/util/classifier.py
@@ -19,18 +19,24 @@ class Classifier(object):
       '''Return a vector containing predicted labels'''
       raise NotImplementedError( "Should have implemented this" )
 
+def opposite_cosine_similarity(a, b):
+      return - np.dot(a,b) / (np.sqrt(np.dot(a,a) * np.dot(b,b)))
 
 
 class KNearestNeighborClassifier(Classifier):
-   '''The metric used is the cosine similarirty'''
-   
-   def __init__(self, K=1):
+   '''
+   - the default 'metric' used is the opposite of the cosine similarirty
+   - the defaut value of K is 1
+   '''
+
+   def __init__(self, K=1, distance=opposite_cosine_similarity):
       self.K = K
+      self.distance = distance
    
    def train(self, labels, data):
       '''Train the classifier'''
       self.labels = np.array(labels)
-      self.data = np.array(data).astype(float) 
+      self.data = np.array(data).astype(float)
 
    def classify(self, data):
       '''Return a vector containing predicted labels'''
@@ -41,9 +47,6 @@ class KNearestNeighborClassifier(Classifier):
       return np.array(predicted_labels)
          
 
-   def _cosinesimilarity(self, a,b):
-      return np.dot(a,b) / (np.sqrt(np.dot(a,a) * np.dot(b,b)))
-           
    def _NN(self, x):
       dist = []
       for i, y in enumerate(self.data):
@@ -51,9 +54,9 @@ class KNearestNeighborClassifier(Classifier):
             print 'KNearestNeighborClassifier: how come this happened ??!! sum -> ', 
             print np.sum(x)
          else:
-            dist.append((self._cosinesimilarity(x,y),self.labels[i]))
+            dist.append((self.distance(x,y),self.labels[i]))
 
-      dist.sort(reverse=True)
+      dist.sort()
       dist = dist[0:min(self.K, len(dist))]
       keys = np.unique(np.array(dist)[:,1])      
       scores = dict(zip(keys, np.zeros(self.K,dtype=np.int)))
