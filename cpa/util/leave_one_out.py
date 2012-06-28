6af73bb7c8cd54588df0b0df633bc116b904c8bb
Vebjorn Ljosa <vebjorn@ljosa.com>
2012-06-28 12:31:30 -0400

    Surrogate variable analysis

--- cpa/util/leave_one_out.py
+++ cpa/util/leave_one_out.py
@@ -3,7 +3,7 @@
 from optparse import OptionParser
 import numpy as np
 import cpa
-from scipy.spatial.distance import cdist
+from scipy.spatial.distance import cdist, cosine
 from .profiles import Profiles
 
 def regroup(profiles, group_name):
@@ -72,6 +72,76 @@ def crossvalidate(profiles, true_group_name, holdout_group_name=None):
                break
     return confusion
 
+
+class NNClassifier(object):
+    def __init__(self, features, labels):
+        assert isinstance(labels, list)
+        assert len(labels) == features.shape[0]
+        self.features = features
+        self.labels = labels
+
+    def classify(self, feature):
+        distances = np.array([cosine(f, feature) for f in self.features])
+        return self.labels[np.argmin(distances)]
+
+
+def crossvalidate(profiles, true_group_name, holdout_group_name=None, 
+                  sva=False, train=NNClassifier):
+    profiles.assert_not_isnan()
+    keys = profiles.keys()
+    true_labels = regroup(profiles, true_group_name)
+    labels = true_labels.values()
+
+    if holdout_group_name:
+        holdouts = regroup(profiles, holdout_group_name)
+    else:
+        holdouts = dict((k, k) for k in keys)
+
+    confusion = {}
+    for ho in set(holdouts.values()):
+        test_set_mask = np.array([tuple(holdouts[k]) == ho for k in keys], 
+                                 dtype=bool)
+        training_features = profiles.data[~test_set_mask, :]
+        test_features = profiles.data[test_set_mask, :]
+        training_labels = [labels.index(true_labels[tuple(k)]) 
+                           for k, m in zip(keys, ~test_set_mask) if m]
+
+        if sva:
+            import pyRserve
+            conn = pyRserve.connect()
+            conn.r.traininglabels = np.array(training_labels, dtype='i4')
+            conn.r.trainData = training_features.ravel().reshape(training_features.shape, order='F').T
+            conn.r.testData = test_features.ravel().reshape(test_features.shape, order='F').T
+            #import pdb; pdb.set_trace()
+            #assert conn.r('trainData[1,2]') == training_features[1, 0]
+            #assert conn.r('testData[1,2]') == test_features[1, 0]
+            conn.r('library(sva)')
+            conn.r('trainData <- as.matrix(trainData)')
+            conn.r('testData <- as.matrix(testData)')
+            conn.r('trainpheno <- data.frame(label=traininglabels)')
+            #conn.r('write.table(trainData, "/tmp/trainData.txt")')
+            #conn.r('write.table(trainpheno, "/tmp/trainpheno.txt")')
+            conn.r('trainMod <- model.matrix(~as.factor(label), trainpheno)')
+            nsv = conn.r('num.sv(trainData, trainMod)')
+            print nsv, 'surrogate variables'
+            conn.r('trainMod0 <- model.matrix(~1, trainpheno)')
+            conn.r('trainSv <- sva(trainData, trainMod, trainMod0)')
+            conn.r('fsvaobj <- fsva(trainData, trainMod, trainSv, testData)')
+            filtered_train = getattr(conn.r, 'fsvaobj$db')
+            filtered_test = getattr(conn.r, 'fsvaobj$new')
+        else:
+            filtered_train = training_features
+            filtered_test = test_features
+
+        model = train(filtered_train, training_labels)
+        for k, f, m in zip(keys, profiles.data, test_set_mask):
+            if not m:
+                continue
+            true = true_labels[k]
+            predicted = labels[model.classify(f)]
+            confusion[true, predicted] = confusion.get((true, predicted), 0) + 1
+    return confusion
+
 def confusion_matrix(confusion, dtype=int):
    labels = set()
    for a, b in confusion.keys():
@@ -103,6 +173,7 @@ def print_confusion(confusion):
 if __name__ == '__main__':
     parser = OptionParser("usage: %prog [-c] [-h HOLDOUT-GROUP] PROPERTIES-FILE PROFILES-FILENAME TRUE-GROUP")
     parser.add_option('-c', dest='csv', help='input and output as CSV', action='store_true')
+    parser.add_option('-s', dest='sva', help='surrogate variable analysis', action='store_true')
     parser.add_option('-H', dest='holdout_group', help='hold out all that map to the same holdout group', action='store')
     options, args = parser.parse_args()
     if len(args) != 3:
@@ -116,5 +187,5 @@ if __name__ == '__main__':
        profiles = Profiles.load(profiles_filename)
 
     confusion = crossvalidate(profiles, true_group_name,
-                              options.holdout_group)
+                              options.holdout_group, sva=options.sva)
     print_confusion(confusion)
