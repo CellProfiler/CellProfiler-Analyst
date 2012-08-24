535065dc006b08e0a5e4a31feba9418737ccf07a
shntnu <shsingh@broadinstitute.org>
2012-08-24 09:37:08 -0400

    profile_percell.py now handles cell_ids, but is no longer backward compatible with old caches that don't have cell_id

--- cpa/profiling/profile_percell.py
+++ cpa/profiling/profile_percell.py
@@ -16,16 +16,16 @@ def _transform_cell_feats((cache_dir, images, normalization_name, output_filenam
         from cpa.profiling.cache import Cache, normalizations
         cache = Cache(cache_dir)
         normalization = normalizations[normalization_name]
-        normalizeddata, normalized_colnames, _ = cache.load(images,
-                                                    normalization=normalization)
+        normalizeddata, normalized_colnames, cell_ids = cache.load(images,
+                                                                   normalization=normalization)
         if len(normalizeddata) == 0:
-            return np.empty(len(normalized_colnames)) * np.nan
+            return np.empty(len(normalized_colnames)+1) * np.nan # +1 is for cell_ids
 
         normalizeddata = normalizeddata[
                 ~np.isnan(np.sum(normalizeddata,1)),:]
 
         if len(normalizeddata) == 0:
-            return np.empty(len(normalized_colnames)) * np.nan
+            return np.empty(len(normalized_colnames)+1) * np.nan # +1 is for cell_ids
 
         # save the features to csv
         import csv
@@ -34,8 +34,9 @@ def _transform_cell_feats((cache_dir, images, normalization_name, output_filenam
         f = open(filename, 'w')
         w = csv.writer(f)
         w.writerow(header)
-        for vector in normalizeddata:
-            w.writerow(tuple(key) + tuple(vector))
+        
+        for i, (cell_id,vector) in enumerate(zip(cell_ids, normalizeddata)):
+            w.writerow(tuple(key) + tuple([cell_id]) + tuple(vector))
         f.close()
         return [-1]
 
@@ -53,7 +54,7 @@ def cell_feats(cache_dir, group_name, filter=None, parallel=Uniprocessing(),
                                              filter=filter)
     variables = normalization(cache).colnames
 
-    header = tuple(colnames_group) + tuple(variables)
+    header = tuple(colnames_group) + tuple(["cell_id"]) + tuple(variables)
     keys = group.keys()
 
     # #HACK!
@@ -69,7 +70,7 @@ def cell_feats(cache_dir, group_name, filter=None, parallel=Uniprocessing(),
                   for g in keys]
 
     if "CPA_DEBUG" in os.environ:
-        DEBUG_NGROUPS = 5
+        DEBUG_NGROUPS = 3
         logging.warning('In debug mode. Using only a few groups (n=%d) to create profile' % DEBUG_NGROUPS)
 
         parameters = parameters[0:DEBUG_NGROUPS]
