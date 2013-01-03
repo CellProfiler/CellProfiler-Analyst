c21872300d013259a46ddc27c926001c4b37ff28
Vebjorn Ljosa <vebjorn@ljosa.com>
2013-01-03 14:15:32 -0500

    cpa.shell script

---
+++ cpa/shell.py
@@ -0,0 +1,27 @@
+#!/bin/bash
+'''
+Load a properties file, then start a shell with certain local
+variables exposed.
+'''
+
+from __future__ import absolute_import
+import code
+import sys
+from optparse import OptionParser
+import cpa
+import cpa.properties
+import cpa.dbconnect
+
+parser = OptionParser("usage: %prog [options] [PROPERTIES-FILE [COMMAND]]")
+options, args = parser.parse_args()
+
+if len(args) > 0:
+    cpa.properties.LoadFile(sys.argv[1])
+
+variables = {'cpa': cpa}
+
+if len(args) == 2:
+    interpreter = code.InteractiveInterpreter(locals=variables)
+    interpreter.runsource(sys.argv[2])
+else:
+    code.interact(local=variables)
