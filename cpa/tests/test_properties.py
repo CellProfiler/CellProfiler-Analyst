cb9827470026932cbb24fd58dd40388cde897bab
Vebjorn Ljosa <vebjorn@ljosa.com>
2014-04-07 12:45:01 -0400

    Commented out test for Issue #24 for now.
    Commented out until we're ready to fix it.

--- cpa/tests/test_properties.py
+++ cpa/tests/test_properties.py
@@ -1,8 +1,9 @@
 import os.path
 import cpa
 
-def test_old_filter():
-    fn = os.path.join(os.path.dirname(__file__),
-                      'test_properties_old_filter.properties')
-    cpa.properties.LoadFile(fn)
+# Issue #24
+# def test_old_filter():
+#     fn = os.path.join(os.path.dirname(__file__),
+#                       'test_properties_old_filter.properties')
+#     cpa.properties.LoadFile(fn)
 
