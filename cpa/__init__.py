ab0b9ea075db10a7da8ef2d7870dd343fe5dd8d9
Vebjorn Ljosa <vebjorn@ljosa.com>
2014-04-18 12:55:08 -0400

    Fix version import

--- cpa/__init__.py
+++ cpa/__init__.py
@@ -1,6 +1,6 @@
 
-import cpa.util.version
-__version__ = cpa.util.version.get_normalized_version()
+from .util.version import get_normalized_version
+__version__ = get_normalized_version()
 import properties
 import dbconnect
 
