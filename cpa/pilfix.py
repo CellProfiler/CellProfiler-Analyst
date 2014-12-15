171654e3e30f08cd33fe4ac7f8717f0132f190aa
Mark Bray <mbray@broadinstitute.org>
2014-12-15 14:57:37 -0500

    Fixing import error (see f70354f)

--- cpa/pilfix.py
+++ cpa/pilfix.py
@@ -1,41 +1,163 @@
+import logging
 import PIL.Image
-import PIL.Hdf5StubImagePlugin
-import PIL.FitsStubImagePlugin
-import PIL.SunImagePlugin
-import PIL.GbrImagePlugin
-import PIL.PngImagePlugin
-import PIL.MicImagePlugin
-import PIL.FpxImagePlugin
-import PIL.PcxImagePlugin
-import PIL.ImImagePlugin
-import PIL.SpiderImagePlugin
-import PIL.PsdImagePlugin
-import PIL.BufrStubImagePlugin
-import PIL.SgiImagePlugin
-import PIL.McIdasImagePlugin
-import PIL.XpmImagePlugin
-import PIL.BmpImagePlugin
-import PIL.TgaImagePlugin
-import PIL.PalmImagePlugin
-import PIL.XVThumbImagePlugin
-import PIL.GribStubImagePlugin
-import PIL.ArgImagePlugin
-import PIL.PdfImagePlugin
-import PIL.ImtImagePlugin
-import PIL.GifImagePlugin
-import PIL.CurImagePlugin
-import PIL.WmfImagePlugin
-import PIL.MpegImagePlugin
-import PIL.IcoImagePlugin
-import PIL.TiffImagePlugin
-import PIL.PpmImagePlugin
-import PIL.MspImagePlugin
-import PIL.EpsImagePlugin
-import PIL.JpegImagePlugin
-import PIL.PixarImagePlugin
-import PIL.PcdImagePlugin
-import PIL.IptcImagePlugin
-import PIL.XbmImagePlugin
-import PIL.DcxImagePlugin
-import PIL.IcnsImagePlugin
-import PIL.FliImagePlugin
+
+try:
+    import PIL.Hdf5StubImagePlugin
+except:
+    logging.info("No HDF5StubImagePlugin")
+try:
+    import PIL.FitsStubImagePlugin
+except:
+    logging.info("No PIL.FitsStubImagePlugin")
+try:
+    import PIL.SunImagePlugin
+except:
+    logging.info("No PIL.SunImagePlugin")
+try:
+    import PIL.GbrImagePlugin
+except:
+    logging.info("No PIL.GbrImagePlugin")
+try:
+    import PIL.PngImagePlugin
+except:
+    logging.info("No PIL.PngImagePlugin")
+try:
+    import PIL.MicImagePlugin
+except:
+    logging.info("No PIL.MicImagePlugin")
+try:
+    import PIL.FpxImagePlugin
+except:
+    logging.info("No PIL.FpxImagePlugin")
+try:
+    import PIL.PcxImagePlugin
+except:
+    logging.info("No PIL.PcxImagePlugin")
+try:
+    import PIL.ImImagePlugin
+except:
+    logging.info("No PIL.ImImagePlugin")
+try:
+    import PIL.SpiderImagePlugin
+except:
+    logging.info("No PIL.SpiderImagePlugin")
+try:
+    import PIL.PsdImagePlugin
+except:
+    logging.info("No PIL.PsdImagePlugin")
+try:
+    import PIL.BufrStubImagePlugin
+except:
+    logging.info("No PIL.BufrStubImagePlugin")
+try:
+    import PIL.SgiImagePlugin
+except:
+    logging.info("No PIL.SgiImagePlugin")
+try:
+    import PIL.McIdasImagePlugin
+except:
+    logging.info("No PIL.McIdasImagePlugin")
+try:
+    import PIL.XpmImagePlugin
+except:
+    logging.info("No PIL.XpmImagePlugin")
+try:
+    import PIL.BmpImagePlugin
+except:
+    logging.info("No PIL.BmpImagePlugin")
+try:
+    import PIL.TgaImagePlugin
+except:
+    logging.info("No PIL.TgaImagePlugin")
+try:
+    import PIL.PalmImagePlugin
+except:
+    logging.info("No PIL.PalmImagePlugin")
+try:
+    import PIL.XVThumbImagePlugin
+except:
+    logging.info("No PIL.XVThumbImagePlugin")
+try:
+    import PIL.GribStubImagePlugin
+except:
+    logging.info("No PIL.GribStubImagePlugin")
+try:
+    import PIL.ArgImagePlugin
+except:
+    logging.info("No PIL.ArgImagePlugin")
+try:
+    import PIL.PdfImagePlugin
+except:
+    logging.info("No PIL.PdfImagePlugin")
+try:
+    import PIL.ImtImagePlugin
+except:
+    logging.info("No PIL.ImtImagePlugin")
+try:
+    import PIL.GifImagePlugin
+except:
+    logging.info("No PIL.GifImagePlugin")
+try:
+    import PIL.CurImagePlugin
+except:
+    logging.info("No PIL.CurImagePlugin")
+try:
+    import PIL.WmfImagePlugin
+except:
+    logging.info("No PIL.WmfImagePlugin")
+try:
+    import PIL.MpegImagePlugin
+except:
+    logging.info("No PIL.MpegImagePlugin")
+try:
+    import PIL.IcoImagePlugin
+except:
+    logging.info("No PIL.IcoImagePlugin")
+try:
+    import PIL.TiffImagePlugin
+except:
+    logging.info("No PIL.TiffImagePlugin")
+try:
+    import PIL.PpmImagePlugin
+except:
+    logging.info("No PIL.PpmImagePlugin")
+try:
+    import PIL.MspImagePlugin
+except:
+    logging.info("No PIL.MspImagePlugin")
+try:
+    import PIL.EpsImagePlugin
+except:
+    logging.info("No PIL.EpsImagePlugin")
+try:
+    import PIL.JpegImagePlugin
+except:
+    logging.info("No PIL.JpegImagePlugin")
+try:
+    import PIL.PixarImagePlugin
+except:
+    logging.info("No PIL.PixarImagePlugin")
+try:
+    import PIL.PcdImagePlugin
+except:
+    logging.info("No PIL.PcdImagePlugin")
+try:
+    import PIL.IptcImagePlugin
+except:
+    logging.info("No PIL.IptcImagePlugin")
+try:
+    import PIL.XbmImagePlugin
+except:
+    logging.info("No PIL.XbmImagePlugin")
+try:
+    import PIL.DcxImagePlugin
+except:
+    logging.info("No PIL.DcxImagePlugin")
+try:
+    import PIL.IcnsImagePlugin
+except:
+    logging.info("No PIL.IcnsImagePlugin")
+try:
+    import PIL.FliImagePlugin
+except:
+    logging.info("No PIL.FliImagePlugin")
