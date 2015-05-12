67dff91f7f9c72eb411a64f910d8d34eb490b818
Shantanu Singh <shsingh@broadinstitute.org>
2015-05-12 18:03:55 -0400

    Added support for pandas in lieu of MySQL database. Partially working code

--- cpa/profiling/cache.py
+++ cpa/profiling/cache.py
@@ -28,8 +28,8 @@ import progressbar
 import numpy as np
 from scipy.stats.stats import scoreatpercentile
 import cpa
-import cpa.dbconnect
-import cpa.util
+# import cpa.dbconnect
+# import cpa.util
 from .normalization import DummyNormalization, normalizations
 
 logger = logging.getLogger(__name__)
@@ -65,7 +65,11 @@ class Cache(object):
                                                 'image_to_plate.pickle')
         self._colnames_filename = os.path.join(self.cache_dir, 'colnames.txt')
         self._counts_filename = os.path.join(self.cache_dir, 'counts.npy')
-
+        self._image_key_columns = options.image_key_columns.split(",")
+        self._plate_id = options.plate_id
+        self._image_table_filename = os.path.join(self.cache_dir, 'image_table.pickle')
+        self._object_table_filename = os.path.join(self.cache_dir, 'object_table.pickle')
+        
     def _image_filename(self, plate, imKey):
         return os.path.join(self.cache_dir, unicode(plate),
                             u'-'.join(map(unicode, imKey)) + '.npz')
@@ -193,29 +197,98 @@ class Cache(object):
     #
 
     def _create_cache(self, resume=False):
-        self._create_cache_colnames(resume)
+        self._create_cache_image_table(resume)
+        self._create_cache_object_table(resume)
+        # self._create_cache_colnames(resume)
         self._create_cache_plate_map(resume)
         self._create_cache_features(resume)
-        self._create_cache_counts(resume)
+        # self._create_cache_counts(resume)
 
-    def _create_cache_colnames(self, resume):
-        """Create cache of column names"""
-        if resume and os.path.exists(self._colnames_filename):
+    def _create_cache_image_table(self, resume):
+        """Create cache of image table"""
+        if resume and os.path.exists(self._image_table_filename):
             return
-        cols = cpa.db.GetColnamesForClassifier()
+            
+        import fnmatch
+        import pandas as pd
+        image_table_csv_l = []
+        for root, dirnames, filenames in os.walk(os.path.join(self.cache_dir, "../")):
+          for filename in fnmatch.filter(filenames, 'image.csv'):
+            image_table_csv_l.append(os.path.join(root, filename))
+
+        image_table_l = []
+        for image_table_csv in image_table_csv_l:
+            #print image_table_csv
+            df = pd.read_csv(image_table_csv, dtype={'ImageNumber': np.int32, 'Metadata_Barcode' : str} )
+            image_table_l.append(df)
+            
+        self._image_table = pd.concat(image_table_l)
+        cpa.util.pickle(self._image_table_filename, self._image_table)
+
+    def _create_cache_object_table(self, resume):
+        """Create cache of object table"""
+        if resume and os.path.exists(self._object_table_filename):
+            return
+
+        import fnmatch
+        import pandas as pd
+        object_table_csv_l = []
+        for root, dirnames, filenames in os.walk(os.path.join(self.cache_dir, "../")):
+          for filename in fnmatch.filter(filenames, 'object.csv'):
+            object_table_csv_l.append(os.path.join(root, filename))
+
+        object_table_l = []
+        for object_table_csv in object_table_csv_l[0:1]:
+            with(open(object_table_csv, "r")) as f:
+                a = f.readline()
+                b = f.readline()
+                
+            header = b.strip().split(",")
+            header_dtype  = dict(zip(header, [np.float32]*len(header)))
+            header_dtype['ImageNumber'] = np.int32
+            header_dtype['ObjectNumber'] = np.int32
+
+            header_new = [x+"_"+y if y not in ['ImageNumber', 'ObjectNumber'] else y for x,y in zip(a.strip().split(","), b.strip().split(",")) ]
+            
+            df = pd.read_csv(object_table_csv, dtype=header_dtype, header=1, names=header_new)
+            object_table_l.append(df)
+            
+            cols = [col for col in header_new if col not in ['ImageNumber', 'ObjectNumber'] ]
+            
+            self._create_cache_colnames1(cols)
+            
+            
+        self._object_table = pd.concat(object_table_l)
+        cpa.util.pickle(self._object_table_filename, self._object_table)
+    
+    def _create_cache_colnames(self, resume):
+        """Create cache of column names - modified to only throw error if file does not exist"""
+        # Check if file exists by trying to open it
+        try:
+            with(open(self._colnames_filename, 'r')) as f:
+                pass
+        except Exception:
+            logger.error('_colnames_filename %s not found.' % _colnames_filename)
+            raise
+        return
+        
+    def _create_cache_colnames1(self, cols):
+        """Create cache of column names"""
+        if os.path.exists(self._colnames_filename):
+            return
         with open(self._colnames_filename, 'w') as f:
             for col in cols:
-                print >>f, col
+                print >>f, col        
 
     def _create_cache_plate_map(self, resume):
         """Create cache of map from image key to plate name"""
         if resume and os.path.exists(self._plate_map_filename):
             return
-        self._cached_plate_map = dict((tuple(row[1:]), row[0])
-                                      for row in cpa.db.execute('select distinct %s, %s from %s'%
-                                                                (cpa.properties.plate_id, 
-                                                                 ', '.join(cpa.dbconnect.image_key_columns()),
-                                                                 cpa.properties.image_table)))
+        from pandasql import sqldf
+        _image_table = self._image_table
+        df = sqldf('select distinct %s, %s from %s'% (self._plate_id, ', '.join(self._image_key_columns), '_image_table'), locals())
+        self._cached_plate_map = dict((tuple(row[1:].tolist()), row[0]) for idx, row in df.iterrows())
+        #from IPython import embed; embed()                                                                 
         cpa.util.pickle(self._plate_map_filename, self._cached_plate_map)
 
     def _create_cache_features(self, resume):
@@ -231,12 +304,24 @@ class Cache(object):
         filename = self._image_filename(plate, image_key)
         if resume and os.path.exists(filename):
             return
+        print filename, image_key
+        return
+        from pandasql import sqldf
+        _object_table = self._object_table
+        
+        _object_table
+        
+        
+        df = sqldf('select %s from %s where %s'% (self._plate_id, ', '.join(self._image_key_columns), '_object_table'), locals())
+
         features = cpa.db.execute("""select %s from %s where %s""" % (
                 ','.join(self.colnames), cpa.properties.object_table, 
                 cpa.dbconnect.GetWhereClauseForImages([image_key])))
+                
         cellids  = cpa.db.execute("""select %s from %s where %s""" % (
                 cpa.properties.object_id, cpa.properties.object_table, 
                 cpa.dbconnect.GetWhereClauseForImages([image_key])))
+                
         np.savez(filename, features=np.array(features, dtype=float), cellids=np.squeeze(np.array(cellids)))
 
     def _create_cache_counts(self, resume):
@@ -264,22 +349,25 @@ def _check_directory(dir, resume):
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
-    parser = OptionParser("usage: %prog [-r] PROPERTIES-FILE CACHE-DIR PREDICATE")
+    parser = OptionParser("usage: %prog [-r] [-c colnames] PLATE-DIR ")
     parser.add_option('-r', dest='resume', action='store_true', help='resume')
+    parser.add_option('-c', dest='colnames', action='store_true', help='file containing column names')
+    parser.add_option('-k', dest='image_key_columns', action='store', help='image_key_columns')
+    parser.add_option('-p', dest='plate_id', action='store', help='plate_id')
+    
     options, args = parser.parse_args()
-    if len(args) != 3:
-        parser.error('Incorrect number of arguments')
-    properties_file, cache_dir, predicate = args
 
-    cpa.properties.LoadFile(properties_file)
+    if len(args) != 1:
+        parser.error('Incorrect number of arguments')
+    cache_dir = os.path.join(args[0], "profiling_params")
 
     _check_directory(cache_dir, options.resume)
 
     cache = Cache(cache_dir)
 
     cache._create_cache(options.resume)
-    if predicate != '':
-        for Normalization in normalizations.values():
-            Normalization(cache)._create_cache(predicate, options.resume)
-    else:
-        print 'Not performing normalization because not predicate was specified.'
+    # if predicate != '':
+    #     for Normalization in normalizations.values():
+    #         Normalization(cache)._create_cache(predicate, options.resume)
+    # else:
+    #     print 'Not performing normalization because not predicate was specified.'
