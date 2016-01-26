19256d8e7e16a50f1619b98bbc996202f70bb635
David Dao <contact.daviddao@gmail.com>
2016-01-25 23:11:23 -0500

    fixed multiple box plots by removing READ_ONLY limitation

--- cpa/boxplot.py
+++ cpa/boxplot.py
@@ -41,7 +41,7 @@ class DataSourcePanel(wx.Panel):
         self.x_columns = [] # column names to plot if selecting multiple columns
 
         self.table_choice = ui.TableComboBox(self, -1, style=wx.CB_READONLY)
-        self.x_choice = ComboBox(self, -1, size=(200,-1), style=wx.CB_READONLY)
+        self.x_choice = ComboBox(self, -1, size=(200,-1))
         self.x_multiple = wx.Button(self, -1, 'select multiple')
         self.group_choice = ComboBox(self, -1, choices=[NO_GROUP]+p._groups_ordered, style=wx.CB_READONLY)
         self.group_choice.Select(0)
