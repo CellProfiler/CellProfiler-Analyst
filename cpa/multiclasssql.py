a54f3e72752328d4b990e631559e0312598ff2be
Jane Hung <jyhung@broadinstitute.org>
2016-05-24 17:12:20 +0800

    Update multiclasssql.py

--- cpa/multiclasssql.py
+++ cpa/multiclasssql.py
@@ -150,9 +150,7 @@ def processData(data):
     data_shape = cell_data.shape
     # if numpy array is already floats, pass; if numpy array contains strings, convert
     cell_data = np.where(cell_data == np.array(None), 0, cell_data)
-    if cell_data.dtype == object:
-        raise Exception('data type is object')
-        
+
     try:
         cell_data = np.reshape(np.genfromtxt(cell_data.ravel(), delimiter=','), data_shape)
         print 'data type 1 ', cell_data.dtype
