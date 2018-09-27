1a42b1c4f0c85a68fb6cc6f6240dfa13bf69949c
cclauss <cclauss@bluewin.ch>
2018-09-27 07:32:28 +0200

    from distutils.errors import DistutilsFileError

--- setup_py2exe.py
+++ setup_py2exe.py
@@ -19,6 +19,7 @@ import subprocess
 import _winreg
 import javabridge
 import bioformats
+from distutils.errors import DistutilsFileError
 from shutil import rmtree
 
 
@@ -89,9 +90,9 @@ OutputBaseFilename=CellProfilerAnalyst_win32_%d
         except WindowsError:
             if key:
                 key.Close()
-            raise distutils.errors.DistutilsFileError("Inno Setup does not seem to be installed properly. "
-                                                      "Specifically, there is no entry in the HKEY_CLASSES_ROOT "
-                                                      "for InnoSetupScriptFile\\shell\\Compile\\command")
+            raise DistutilsFileError("Inno Setup does not seem to be installed properly. "
+                                     "Specifically, there is no entry in the HKEY_CLASSES_ROOT "
+                                     "for InnoSetupScriptFile\\shell\\Compile\\command")
 
 # if os.path.exists('build'):
 #    raise Exception("Please delete the build directory before running setup.")
