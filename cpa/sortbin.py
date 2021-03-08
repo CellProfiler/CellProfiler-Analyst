98f3ebdab42f2b89ae81a5d640fa5a2f9659cff8
David Stirling <dstirling@broadinstitute.org>
2021-03-08 13:00:43 -0500

    Whitespace cleanup

--- cpa/sortbin.py
+++ cpa/sortbin.py
@@ -83,7 +83,7 @@ class SortBin(wx.ScrolledWindow):
     def __init__(self, parent, chMap=None, label='', classifier=None, parentSizer=None):
         wx.ScrolledWindow.__init__(self, parent)
         self.SetDropTarget(SortBinDropTarget(self))
-        
+
         self.label           = label
         self.parentSizer     = parentSizer
         self.tiles           = []
@@ -99,7 +99,7 @@ class SortBin(wx.ScrolledWindow):
             self.chMap = chMap
         else:
             self.chMap = p.image_channel_colors
-        
+
         self.SetBackgroundColour('#000000')
         self.sizer = ImageTileSizer()
         self.SetSizer(self.sizer)
@@ -108,7 +108,7 @@ class SortBin(wx.ScrolledWindow):
         (w,h) = self.sizer.GetSize()
         self.SetScrollbars(20,20,w/20,h/20,0,0)
         self.EnableScrolling(xScrolling=False, yScrolling=True)
-                
+
         self.Bind(wx.EVT_LEFT_DOWN, self.OnLeftDown)
         self.Bind(wx.EVT_LEFT_UP, self.OnLeftUp)
         self.Bind(wx.EVT_MOTION, self.OnMotion)
@@ -120,12 +120,12 @@ class SortBin(wx.ScrolledWindow):
         self.Bind(wx.EVT_PAINT, self.OnPaint)
 
         tilecollection.EVT_TILE_UPDATED(self, self.OnTileUpdated)
-    
+
         self.CreatePopupMenu()
 
     def __str__(self):
         return 'Bin %s with %d objects'%(self.label, len(self.sizer.GetChildren()))
-        
+
     def CreatePopupMenu(self):
         popupMenuItems = ['View full images of selected',
                           'Select all\tCtrl+A',
@@ -141,7 +141,7 @@ class SortBin(wx.ScrolledWindow):
             self.popupItemIndexById[id] = i
             self.popupMenu.Append(id,item)
         self.popupMenu.Bind(wx.EVT_MENU,self.OnSelectFromPopupMenu)
-        
+
     def OnKey(self, evt):
         ''' Keyboard shortcuts '''
         if evt.GetKeyCode() in [wx.WXK_DELETE, wx.WXK_BACK]:        # delete
@@ -154,22 +154,22 @@ class SortBin(wx.ScrolledWindow):
             elif evt.GetKeyCode() == ord('I'):
                 self.InvertSelection()
         evt.Skip()
-            
+
     def OnRightDown(self, evt):
         ''' On right click show popup menu. '''
         self.PopupMenu(self.popupMenu, evt.GetPosition())
-    
+
     def OnSelectFromPopupMenu(self, evt):
         ''' Handles selections from the popup menu. '''
         choice = self.popupItemIndexById[evt.GetId()]
         if choice == 0:
             for key in self.SelectedKeys():
                 if self.classifier:
-                    imViewer = imagetools.ShowImage(key[:-1], self.chMap[:], 
-                                    parent=self.classifier, 
-                                    brightness=self.classifier.brightness, 
-                                    scale=self.classifier.scale,
-                                    contrast=self.classifier.contrast)
+                    imViewer = imagetools.ShowImage(key[:-1], self.chMap[:],
+                                                    parent=self.classifier,
+                                                    brightness=self.classifier.brightness,
+                                                    scale=self.classifier.scale,
+                                                    contrast=self.classifier.contrast)
                 else:
                     imViewer = imagetools.ShowImage(key[:-1], self.chMap[:], parent=self)
 
@@ -186,10 +186,10 @@ class SortBin(wx.ScrolledWindow):
             self.classifier.RenameClass(self.label)
         elif choice == 6:
             self.classifier.RemoveSortClass(self.label)
-    
+
     def AddObject(self, obKey, chMap=None, priority=1, pos='first'):
         self.AddObjects([obKey], chMap, priority, pos)
-                        
+
     def AddObjects(self, obKeys, chMap=None, priority=1, pos='first', display_whole_image=False, srcID=None):
         if chMap is None:
             chMap = p.image_channel_colors
@@ -242,7 +242,7 @@ class SortBin(wx.ScrolledWindow):
     def RemoveKey(self, obKey):
         ''' Removes the specified tile. '''
         self.RemoveKeys([obKey])
-        
+
     def RemoveKeys(self, obKeys):
         ''' Removes the specified tile. '''
         for t in self.tiles:
@@ -260,7 +260,7 @@ class SortBin(wx.ScrolledWindow):
             wx.CallAfter(tile.Destroy)
         wx.CallAfter(self.UpdateSizer)
         self.UpdateQuantity()
-    
+
     def Clear(self):
         self.RemoveKeys(self.GetObjectKeys())
 
@@ -268,7 +268,7 @@ class SortBin(wx.ScrolledWindow):
         for t in self.tiles:
             if t.obKey == obkey and t.selected:
                 return t
-        
+
     def ReceiveDrop(self, srcID, obKeys):
         # Generate a closure to fix the issue, that images dragged into the own bin are deleted
         # Add back the deleted images after deletion
@@ -290,17 +290,17 @@ class SortBin(wx.ScrolledWindow):
         self.SetFocusIgnoringChildren() # prevent children from getting focus (want bin to catch key events)
         #self.classifier.UpdateTrainingSet() # Update TrainingSet after each drop (very slow)
         return wx.DragMove
-        
+
     def MapChannels(self, chMap):
         ''' Recalculates the displayed bitmap for all tiles in this bin. '''
         self.chMap = chMap
         for tile in self.tiles:
             tile.MapChannels(self.chMap)
-            
+
     def SelectedKeys(self):
         ''' Returns the keys of currently selected tiles on this bin. '''
         return [tile.obKey for tile in self.tiles if tile.selected]
-    
+
     def Selection(self):
         ''' Returns the currently selected tiles on this bin. '''
         tiles = []
@@ -308,26 +308,26 @@ class SortBin(wx.ScrolledWindow):
             if tile.selected:
                 tiles.append(tile)
         return tiles
-    
+
     def GetObjectKeys(self):
         return [tile.obKey for tile in self.tiles]
-        
+
     def GetNumberOfTiles(self):
         return len(self.tiles)
-    
+
     def GetNumberOfSelectedTiles(self):
         return len(self.SelectedKeys())
-        
+
     def SelectAll(self):
         ''' Selects all tiles on this bin. '''
         for tile in self.tiles:
             tile.Select()
-        
+
     def DeselectAll(self):
         ''' Deselects all tiles on this bin. '''
         for tile in self.tiles:
             tile.Deselect()
-            
+
     def InvertSelection(self):
         ''' Inverts the selection. '''
         for t in self.tiles:
@@ -404,7 +404,7 @@ class SortBin(wx.ScrolledWindow):
           StaticBox label to contain the current object count.
         '''
         empty = len(self.tiles) == 0
-        if (empty and not self.empty) or (not empty and self.empty): 
+        if (empty and not self.empty) or (not empty and self.empty):
             self.empty = empty
             event = wx.PyCommandEvent(EVT_QUANTITY_CHANGED.typeId, self.GetId())
             self.GetEventHandler().ProcessEvent(event)
